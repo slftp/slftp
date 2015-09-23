78d45c9482bd1b2f07672fc1341d499715355690
DEADC0DE <Dreamboxx@protonmail.com>
2015-09-23 21:13:59 +0200

    reformat....

--- globalskipunit.pas
+++ globalskipunit.pas
@@ -7,12 +7,12 @@ uses classes, Regexpr;
 procedure Initglobalskiplist;
 procedure Uninitglobalskiplist;
 
-function Rehashglobalskiplist:boolean;
+function Rehashglobalskiplist: boolean;
 
-function CheckForBadAssGroup(rls:string):boolean;
+function CheckForBadAssGroup(rls: string): boolean;
 
-
-var globalgroupskip:TStringlist;
+var
+  globalgroupskip: TStringlist;
 
 implementation
 
@@ -20,50 +20,62 @@ uses debugunit, sysutils, irc;
 
 procedure Initglobalskiplist;
 begin
- Debug(dpSpam, 'global_skip_group', 'Loading up Global group skiplist..');
-  globalgroupskip:=TStringlist.Create;
+  Debug(dpSpam, 'global_skip_group', 'Loading up Global group skiplist..');
+  globalgroupskip := TStringlist.Create;
   Rehashglobalskiplist;
 end;
 
 procedure Uninitglobalskiplist;
 begin
-if globalgroupskip <> nil then begin
-globalgroupskip.free;
-globalgroupskip:=nil;
-end;
-end;
-
-function Rehashglobalskiplist:boolean;
-var x:TStringlist;
-begin
-  x:=TStringlist.Create;
-  try
-  x.LoadFromFile(ExtractFilePath(ParamStr(0))+'slftp.skipgroups');
-  globalgroupskip.Clear;
-  globalgroupskip.Delimiter:=' ';
-  globalgroupskip.DelimitedText:=x.text;
-  x.free;
-  result:=True;
- Debug(dpSpam, 'global_skip_group', 'Stopped!');
-  except on E: Exception do
-  result:=False;
+  if globalgroupskip <> nil then
+  begin
+    globalgroupskip.free;
+    globalgroupskip := nil;
   end;
 end;
 
-function CheckForBadAssGroup(rls:string):boolean;
-var r:TRegexpr;//i:integer;
+function Rehashglobalskiplist: boolean;
+var
+  x: TStringlist;
 begin
-result:=False;
-  r:=TRegexpr.Create;
-  r.Expression:='\-([^\-]+)$';
+  result := False;
+  try
+    x := TStringlist.Create;
+    try
+      x.LoadFromFile(ExtractFilePath(ParamStr(0)) + 'slftp.skipgroups');
+      globalgroupskip.Clear;
+      globalgroupskip.Delimiter := ' ';
+      globalgroupskip.DelimitedText := x.text;
+      result := True;
+    finally
+      x.free;
+    end;
+  except on E: Exception do
+    begin
+      Debug(dpError, section, format('Exception in Rehashglobalskiplist: %s',
+        [E.Message]));
+      result := False;
+    end;
+
+  end;
+end;
+
+function CheckForBadAssGroup(rls: string): boolean;
+var
+  r: TRegexpr; //i:integer;
+begin
+  result := False;
+  r := TRegexpr.Create;
+  r.Expression := '\-([^\-]+)$';
   if r.Exec(rls) then
   begin
     if globalgroupskip.IndexOf(r.Match[1]) <> -1 then
-      result:=True
+      result := True
     else
-      result:=False;
+      result := False;
   end;
   r.free;
 end;
 
 end.
+
