6a3cb50eb1fc0083122b87cb4e2768e982f03ee7
slftp <slftp@slftp>
2016-03-01 11:45:33 +0100

    0000042: unable to identify tv show name for the release without S## (E##)?

--- dbtvinfo.pas
+++ dbtvinfo.pas
@@ -152,7 +152,7 @@ begin
       episode := StrToIntDef(rx.Match[3], 0);
     end;
 
-    rx.Expression := '(.*)[\._-](S(\d{1,3}))?(\.?([DE]|EP|Episode|Part)(\d{1,4})\w?(E(\d{1,4}))?)?[\._-](.*)';
+    rx.Expression := '(.*)[\._-](S(\d{1,3}))?(\.?([DE]|EP|Episode|Part\.?)(\d{1,4})\w?(E(\d{1,4}))?)?[\._-](.*)';
     if rx.Exec(rip) then
     begin
       showname := rx.Match[1];
