aaa81fe16e8661c00423dacdbb5c34388c445956
slftp <slftp@slftp>
2016-10-15 21:50:32 +0200

    refactor(TMyIrcThread.chanpart): Try..finally + channels.BeginUpdate before try block
    We should do channels.BeginUpdate before the try block and in finally
    part is the channels.EndUpdate;

--- irc.pas
+++ irc.pas
@@ -1094,8 +1094,8 @@ begin
 
     irc_Addadmin('Try to part ' + channels.Names[i]);
 
+    channels.BeginUpdate;
     try
-      channels.BeginUpdate;
       channels.Delete(i);
     finally
       channels.EndUpdate;
@@ -1107,15 +1107,17 @@ begin
   end;
 
   x := TStringList.Create;
-  x.DelimitedText := channels.Values[chan];
-  i := x.IndexOf(nick);
-  if i <> -1 then
-    x.Delete(i);
-  channels.Values[chan] := x.DelimitedText;
+  try
+    x.DelimitedText := channels.Values[chan];
+    i := x.IndexOf(nick);
+    if i <> -1 then
+      x.Delete(i);
+    channels.Values[chan] := x.DelimitedText;
 
-  status := 'online (' + channelsList + ')';
-
-  x.Free;
+    status := 'online (' + channelsList + ')';
+  finally
+    x.Free;
+  end;
 end;
 
 procedure TMyIrcThread.chanjoin(chan, nick: AnsiString);
