ac46e1526a16f58160bd7e41258c52dc9527410c
Dreamboxx <Dreamboxx@protonmail.com>
2017-04-03 19:34:58 +0200

    style(dirlist,pazo): Formating

--- pazo.pas
+++ pazo.pas
@@ -591,14 +591,13 @@ begin
           if ((dstdl.parent <> nil) and (dstdl.entries.Count > 0)) then
             Continue;
 
-          //          if ((dde <> nil) and (dde.tradeCount > config.ReadInteger('taskrace', 'maxsame_trade', 100))) then Continue;
+          //if ((dde <> nil) and (dde.tradeCount > config.ReadInteger('taskrace', 'maxsame_trade', 100))) then Continue;
 
           Debug(dpSpam, section, '%s :: Tuzelj, checking routes from %s to %s :: Adding RACE task on %s %s', [dir, Name, dst.Name, dst.Name, de.filename]);
           pr := TPazoRaceTask.Create(netname, channel, Name, dst.Name, pazo, dir, de.filename, de.filesize, destinationRanks[i]);
 
           if (AnsiLowerCase(de.Extension) = '.sfv') then
             pr.IsSfv := True;
-
           if (AnsiLowerCase(de.Extension) = '.nfo') then
             pr.IsNfo := True;
 
