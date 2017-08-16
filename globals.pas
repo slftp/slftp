064a6d0a35a5e9f7b90a1b28e8d982a400d31f00
DEADC0DE <Dreamboxx@protonmail.com>
2017-08-16 17:10:27 +0200

    refactor(SiteColors): Global values for sitecolors
    Adds global values for sitecolors we can use in srccode to prevent
    mixing of different colors for same site status.

--- globals.pas
+++ globals.pas
@@ -5,6 +5,12 @@ interface
 type
   TDirType = (IsUnknown, IsMain, IsMultiCD, IsSample, IsProof, IsCovers, IsSubs);
 
+const
+  SiteColorOnline = 'c3'; //< Green color for sstUp @link(sitesunit.TSiteStatus)
+  SiteColorOffline = 'c4'; //< Light Red color for sstDown @link(sitesunit.TSiteStatus)
+  SiteColorPermdown = 'c5'; //< Brown color if site is set to permdown
+  SiteColorUnknown = 'c14'; //< Grey color for sstUnknown @link(sitesunit.TSiteStatus)
+
 implementation
 
 end.
