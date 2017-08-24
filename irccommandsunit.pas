5b8e3905af229db5cbe4b1f85a66ce491cb02010
slftp <slftp@slftp>
2017-08-24 08:23:28 +0200

    fix(!status): Re-enable news status line
    Error was due to a somehow corrupt slftp.news file.
    
    Maybe we should add a note somewhere that if this occurs the slftp.news
    should be removed.

--- irccommandsunit.pas
+++ irccommandsunit.pas
@@ -11263,8 +11263,7 @@ var
 begin
   IrcUptime(Netname, Channel, '');
 
-  // Throws an ECompressionError ?!
-  //irc_addtext(Netname, Channel, SlftpNewsStatus);
+  irc_addtext(Netname, Channel, SlftpNewsStatus);
 
   irc_addtext(Netname, Channel, '<b>Knowledge Base</b>: %d Rip''s in mind', [kb_list.Count]);
   irc_addtext(Netname, Channel, TheTVDbStatus);
