19bf774334c486d1318715999038a97366c2e1ce
DEADC0DE <Dreamboxx@protonmail.com>
2017-09-14 18:05:20 +0200

    fix(TheTVDb): Invalid date caused by wrong date order
    Need to be in Year-Month-Day order!

--- globals.pas
+++ globals.pas
@@ -15,7 +15,7 @@ const
   SiteColorUnknown = 'c14'; //< Grey color for sstUnknown @link(sitesunit.TSiteStatus)
 
 var
-  formatSettings: TFormatSettings;
+  formatSettings: TFormatSettings; //< TFormatSettings for date/time handling (formatted as in official or technical documents)
 
 procedure InitGlobalValues;
 
@@ -28,7 +28,7 @@ begin
   {$ELSE}
     formatSettings := DefaultFormatSettings;
   {$ENDIF}
-  formatSettings.ShortDateFormat := 'yyyy-mm-dd';
+  formatSettings.ShortDateFormat := 'yyyy-mm-dd'; // Year-Month-Day order
   formatSettings.ShortTimeFormat := 'hh:mm';
   formatSettings.DateSeparator := '-';
   formatSettings.TimeSeparator := ':';
