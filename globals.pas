fa19e437588d4f6af1a7621d0cd0b85e26be7721
slftp <slftp@slftp>
2017-09-19 12:21:41 +0200

    Revert "fix(TheTVDB): Proper global TFormatSettings"
    This reverts commit 009c6b7c40ccd9539ab8cf97f9f85e22993968c6.

--- globals.pas
+++ globals.pas
@@ -2,9 +2,6 @@ unit globals;
 
 interface
 
-uses
-  SysUtils;
-
 type
   TDirType = (IsUnknown, IsMain, IsMultiCD, IsSample, IsProof, IsCovers, IsSubs);
 
@@ -14,24 +11,6 @@ const
   SiteColorPermdown = 'c5'; //< Brown color if site is set to permdown
   SiteColorUnknown = 'c14'; //< Grey color for sstUnknown @link(sitesunit.TSiteStatus)
 
-var
-  formatSettings: TFormatSettings;
-
-procedure InitGlobalValues;
-
 implementation
-
-procedure InitGlobalValues;
-begin
-  {$IFDEF MSWINDOWS}
-    GetLocaleFormatSettings(1033, formatSettings);
-  {$ELSE}
-    formatSettings := DefaultFormatSettings;
-  {$ENDIF}
-  formatSettings.ShortDateFormat := 'yyyy-mm-dd';
-  formatSettings.ShortTimeFormat := 'hh:mm';
-  formatSettings.DateSeparator := '-';
-  formatSettings.TimeSeparator := ':';
-end;
 
 end.
