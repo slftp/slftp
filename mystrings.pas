c84cbb41e56c75eb4386d9097413119345042fe8
DEADC0DE <Dreamboxx@protonmail.com>
2017-10-02 12:36:39 +0200

    fix(mystrings.pas): fix fuckup

--- mystrings.pas
+++ mystrings.pas
@@ -34,7 +34,7 @@ unit mystrings;
 
 interface
 
-uses Classes, RegExpr;
+uses Classes;
 
 function onlyEnglishAlpha(s: AnsiString): AnsiString;
 function ArrayTextContainesIndexText(const AText, AValues: array of ansistring): Boolean;
@@ -77,7 +77,6 @@ function MyTimeToStr(x: TDateTime): AnsiString;
 function MyStrToTime(x: AnsiString): TDateTime;
 function MyDateToStr(x: TDateTime): AnsiString;
 function MyStrToDate(x: AnsiString): TDateTime;
-function MyStrToDateTime(x: AnsiString): TDateTime;
 function NoToTime(x: integer): AnsiString; overload;
 function NoToTime(s: AnsiString): AnsiString; overload;
 function Szovegge(szam: integer): AnsiString; overload;
@@ -661,20 +660,6 @@ begin
   s  := StrToIntDef(Copy(x, 18, 2), 0);
   if not TryEncodeDateTime(y, m, d, h, mm, s, 0, Result) then
     Result := 0;
-end;
-
-function MyStrToDateTime(x: AnsiString): TDateTime;
-var
-  y, m, d, h, mm, s: integer;
-  rx: TRegexpr;
-begin
-  rx := TRegexpr.Create;
-  try
-  // is it YYYY MM DD
-
-  finally
-    rx.Free;
-  end;
 end;
 
 function NoToTime(x: integer): AnsiString;
