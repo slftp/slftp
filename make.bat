4643b8d0c37eafa3a02c55317731c4db241a50cb
slftp <slftp@slftp>
2018-03-31 17:12:47 +0200

    fix(make.bat): Fixed compilation flags for Delphi Unicode

--- make.bat
+++ make.bat
@@ -5,6 +5,7 @@ REM
 set CC=C:\Program Files (x86)\CodeGear\RAD Studio\5.0\bin\dcc32.exe
 set CC_ND_32=C:\Program Files (x86)\Embarcadero\Studio\19.0\bin\dcc32.exe
 set CC_ND_64=C:\Program Files (x86)\Embarcadero\Studio\19.0\bin\dcc64.exe
+set CC_ND_EXTRAS=-NSWinapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;System;Xml;Data;Datasnap;Web;Soap;DUnitX.MemoryLeakMonitor
 set CFLAGS=-B -$O+,C+,D-,L-
 set CDBFLAGS=-B -$O+,C+,D+,L+
 set CINCLUDES=-Ulibs/FastMM4 -Ulibs/BeRoHighResolutionTimer -Ulibs/FLRE -Ulibs/rcmdline -Ulibs/DFFLibV15_UIntList -Ulibs/lkJSON -Ulibs/TRegExpr -Ulibs/pasmp -Ulibs/Compvers -Ulibs/ctypes -Ulibs/Indy10_5448/Core -Ulibs/Indy10_5448/Protocols -Ulibs/Indy10_5448/System -Ulibs/LibTar
@@ -24,6 +25,7 @@ goto :error
 del /q *.exe *.dcu
 echo Compiling RELEASE Win32 slftp.exe
 "%CC%" %CFLAGS% %CINCLUDES% slftp.dpr
+echo "%CC%" %CFLAGS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    exit /b %errorlevel%
@@ -33,6 +35,7 @@ goto :eof
 :slftp_debug
 del /q *.exe *.dcu
 echo Compiling DEBUG Win32 slftp.exe
+echo "%CC%" %CDBFLAGS% %CINCLUDES% slftp.dpr
 "%CC%" %CDBFLAGS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
@@ -43,7 +46,8 @@ goto :eof
 :slftp_nd_32
 del /q *.exe *.dcu
 echo Compiling NEWDELPHI RELEASE Win32 slftp.exe
-"%CC_ND_32%" %CFLAGS% %CINCLUDES% slftp.dpr
+echo "%CC_ND_32%" %CFLAGS% %CC_ND_EXTRAS% %CINCLUDES% slftp.dpr
+"%CC_ND_32%" %CFLAGS% %CC_ND_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    exit /b %errorlevel%
@@ -53,7 +57,7 @@ goto :eof
 :slftp_nd_64
 del /q *.exe *.dcu
 echo Compiling NEWDELPHI RELEASE Win64 slftp.exe
-"%CC_ND_64%" %CFLAGS% %CINCLUDES% slftp.dpr
+"%CC_ND_64%" %CFLAGS% %CC_ND_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    exit /b %errorlevel%
@@ -63,7 +67,7 @@ oto :eof
 :slftp_nd_32_debug
 del /q *.exe *.dcu
 echo Compiling NEWDELPHI DEBUG Win32 slftp.exe
-"%CC_ND_32%" %CDBFLAGS% %CINCLUDES% slftp.dpr
+"%CC_ND_32%" %CDBFLAGS% %CC_ND_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    exit /b %errorlevel%
@@ -73,7 +77,7 @@ goto :eof
 :slftp_nd_64_debug
 del /q *.exe *.dcu
 echo Compiling NEWDELPHI DEBUG Win64 slftp.exe
-"%CC_ND_64%" %CDBFLAGS% %CINCLUDES% slftp.dpr
+"%CC_ND_64%" %CDBFLAGS% %CC_ND_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
    echo Failure Reason Given is %errorlevel%
    exit /b %errorlevel%
