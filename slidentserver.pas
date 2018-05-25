328e7e8d1c9e332c3bfe031667fb80b7f5055101
slftp <slftp@slftp>
2018-05-25 20:40:21 +0200

    fix(slidentserver.pas): Missing update of function header

--- slidentserver.pas
+++ slidentserver.pas
@@ -5,23 +5,21 @@ interface
 uses sltcp;
 
 type
-
   TslIdentLookup = class(TslSCHThread)
     procedure RealExecute(); override;
   end;
+  
   TslIdentServer = class(TslTCPServer)
   public
-    function Lookup(ip: String; localport, remoteport: Integer): String; virtual; abstract;
+    function Lookup(const ip: String; const localport, remoteport: Integer): String; virtual; abstract;
     constructor Create;
   end;
-
 
 implementation
 
 uses SysUtils;
 
 { TslIdentServer }
-
 
 (*
 56646,13307
@@ -30,23 +28,23 @@ uses SysUtils;
 *)
 
 procedure TslIdentLookup.RealExecute;
-var s, ossz, username: String;
-    localport, remoteport: Integer;
-    i: Integer;
+var
+  s, ossz, username: String;
+  localport, remoteport: Integer;
+  i: Integer;
 begin
   if not client.ReadLn(s, ossz, 5000) then exit;
 
-  i:= Pos(',', s);
+  i := Pos(',', s);
   if i = 0 then exit;
-  localport:= StrToIntDef(Copy(s, 1, i-1), -1);
-  remoteport:= StrToIntDef(Copy(s, i+1, 8), -1);
+  localport := StrToIntDef(Copy(s, 1, i-1), -1);
+  remoteport := StrToIntDef(Copy(s, i+1, 8), -1);
 
   if ((localport < 0) or (remoteport < 0)) then exit;
 
-  username:= TslIdentServer(ServerThread.Server).Lookup(client.slSocket.peerip, localport, remoteport);
+  username := TslIdentServer(ServerThread.Server).Lookup(client.slSocket.peerip, localport, remoteport);
 
   client.WriteLn(Format('%d,%d : USERID : UNIX : %s', [localport, remoteport, username]))
-
 end;
 
 constructor TslIdentServer.Create;
