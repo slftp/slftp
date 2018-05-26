9af83eafe01c506c8d666714226cc8a43dc9517e
slftp <slftp@slftp>
2018-05-27 00:02:38 +0200

    feat(BlowfishCBC): Support for Blowfish CBC mode
    Adds support for Blowfish CBC irc channel decryption/encryption.
    
    To use it you have to add blowfish key with cbc:MYKEY as
    in mIRC with FiSH 10.
    Code bases on C++ implementation from FiSH 10.
    
    Closes #124

--- ircblowfish.pas
+++ ircblowfish.pas
@@ -27,9 +27,20 @@ type
     function HasKey(key: String): Boolean;
   end;
 
-function irc_encrypt(netname, channel, dText: String; include_ok: Boolean = False): String;
+{ function to decide which encryption mode should be used
+  For more infos: http://blog.bjrn.se/2009/01/proposal-for-better-irc-encryption.html }
+function irc_encrypt(const netname, channel, dText: String; include_ok: Boolean = False): String;
+
+{ Encrypt decrypted dText in CBC mode }
+function irc_cbc_encrypt(const dText: String; const BlowfishKey: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF}): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+{ Decrypt encrypted eText in CBC mode }
+function irc_cbc_decrypt(const netname, channel, eText: String): String;
+
+{ Encrypt decrypted dText in ECB mode }
+function irc_ecb_encrypt(dText: String; BlowfishKeyData: TBlowfishData): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
 { Decrypt encrypted eText in ECB mode }
 function irc_ecb_decrypt(const netname, channel, eText: String): String;
+
 function irc_RegisterChannel(const netname, channel, blowkey: String; chankey: String = ''; inviteonly: Boolean = False; cbc: Boolean = False): TIrcBlowkey;
 function FindIrcBlowfish(const netname, channel: String; acceptdefault: Boolean = True): TIrcBlowkey;
 procedure IrcblowfishInit;
@@ -41,7 +52,7 @@ var
 implementation
 
 uses
-  SysUtils, console, debugunit;
+  SysUtils, Classes, console, debugunit, IdSSLOpenSSLHeaders, Base64OpenSSL;
 
 const
   B64: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF} = './0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -66,6 +77,7 @@ begin
     Result := s[1];
 end;
 
+{ non-standard base64 implementation for ECB mode }
 function bytetoB64(const ec: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF}): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
 var
   dc: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
@@ -111,6 +123,7 @@ begin
   Result := dc;
 end;
 
+{ non-standard base64 implementation for ECB mode }
 function B64tobyte(const ec: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF}): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
 var
   dc: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
@@ -206,14 +219,11 @@ begin
     Result := nil;
 end;
 
-function irc_encrypt(netname, channel, dText: String; include_ok: Boolean = False): String;
+function irc_encrypt(const netname, channel, dText: String; include_ok: Boolean = False): String;
 var
-  temp, eText: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
-  i: Integer;
   bf: TIrcBlowkey;
 begin
   Result := '';
-  eText := '';
 
   if (dText = '') then
     exit;
@@ -225,34 +235,328 @@ begin
     exit;
   end;
 
+  if bf.cbc then
+  begin
+    // do CBC encryption
+    Result := irc_cbc_encrypt(dText, bf.blowkey);
+  end
+  else
+  begin
+    // do ECB encryption
+    Result := irc_ecb_encrypt(dText, bf.KeyData);
+  end;
+
+  if include_ok then
+    {$IFDEF UNICODE}
+      Result := UTF8ToString('+OK ' + Result)
+    {$ELSE}
+      Result := '+OK ' + Result
+    {$ENDIF}
+  else
+    {$IFDEF UNICODE}
+      Result := UTF8ToString(Result);
+    {$ELSE}
+      Result := Result;
+    {$ENDIF}
+end;
+
+{ CBC mode }
+function BlowfishCipherWalk(aCTX: PEVP_CIPHER_CTX; aBufIn: PAnsiChar; aInSize: integer; out aOut: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF}): boolean;
+var
+  fSuccess: boolean;
+  fBytesLeft: integer;
+  fBufPtr: PAnsiChar;
+  fInSize: integer;
+  fTmpBuf: array[0..255] of Byte;
+  fOutLen: integer;
+  fArgOutLen: integer;
+begin
+  Result := False;
+
+  fSuccess := False;
+  fOutLen := 255;
+
+  fBytesLeft := aInSize;
+  fBufPtr := aBufIn;
+
+  while (fBytesLeft > 0) do
+  begin
+    if (fBytesLeft > 256) then
+    begin
+      fInSize := 256;
+    end
+    else
+    begin
+      fInSize := fBytesLeft;
+    end;
+
+    if (EVP_CipherUpdate(aCTX, @fTmpBuf[0], @fOutLen, fBufPtr, fInSize) <> 1) then
+    begin
+      Debug(dpError, section, '[FiSH] EVP_CipherUpdate failed for CBC!');
+      exit;
+    end;
+
+    aOut := '';
+    fArgOutLen := Length(aOut);
+
+    SetLength(aOut, fArgOutLen + fOutLen);
+    Move(fTmpBuf[0], aOut[fArgOutLen + 1], fOutLen);
+
+    Dec(fBytesLeft, fInSize);
+    Inc(fBufPtr, fInSize);
+  end;
+
+  fSuccess := (EVP_CipherFinal_ex(aCTX, @fTmpBuf[0], @fOutLen) = 1);
+
+  if (fSuccess) then
+  begin
+    fArgOutLen := Length(aOut);
+
+    SetLength(aOut, fArgOutLen + fOutLen);
+    Move(fTmpBuf[0], aOut[fArgOutLen + 1], fOutLen);
+  end;
+
+  Result := fSuccess;
+end;
+
+function irc_cbc_encrypt(const dText: String; const BlowfishKey: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF}): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+const
+  IV: array[0..7] of Byte = ($00, $00, $00, $00, $00, $00, $00, $00);
+var
+  fKeyLen: integer;
+  fCTX: EVP_CIPHER_CTX;
+  fInBufSize: integer;
+  fInBufSizeMod: integer;
+  fInBuf: array of Byte;
+  fRealIV: array[0..7] of Byte;
+  fSuccess: boolean;
+  eText, eTextBase64: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+begin
+  Result := '';
+  eText := '';
+  eTextBase64 := '';
+
+  FillChar(fRealIV[0], High(fRealIV), 0);
+
+  fKeyLen := Length(BlowfishKey);
+  if (fKeyLen >= 56) then
+    fKeyLen := 56;
+
+  {* init struct for encryption *}
+  EVP_CIPHER_CTX_init(@fCTX);
+  if (EVP_CipherInit_ex(@fCTX, EVP_bf_cbc(), nil, nil, nil, 1) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] First EVP_CipherInit_ex failed for CBC encryption!');
+    exit;
+  end;
+
+  {* set options *}
+  EVP_CIPHER_CTX_set_key_length(@fCTX, fKeyLen);
+  // disable auto padding. Required for Mircryption compatibility.
+  EVP_CIPHER_CTX_set_padding(@fCTX, 0);
+
+  {* actually initialize session context *}
+  // 1 for encryption mode
+  if (EVP_CipherInit_ex(@fCTX, nil, nil, @BlowfishKey[1], @IV[0], 1) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] First EVP_CipherInit_ex failed for CBC encryption!');
+    exit;
+  end;
+
+  // prepare buffers
+  fInBufSize := Length(dText);
+  fInBufSizeMod := fInBufSize mod 8;
+  if (fInBufSizeMod <> 0) then
+  begin
+    Inc(fInBufSize, 8 - fInBufSizeMod);
+  end;
+  Inc(fInBufSize, 8); // for the IV data
+
+  SetLength(fInBuf, fInBufSize);
+  // important for padding
+  FillChar(fInBuf[0], fInBufSize, 0);
+
+  // for some f*cked up reason, Mircryption's CBC blowfish does not use an
+  // explicit IV, but prepends 8 bytes of random data to the actual string
+  // instead, so we have to do this too... }
+  {$IFDEF MSWINDOWS}
+    // generate IV using screen/user input
+    RAND_screen();
+  {$ELSE}
+    // TODO: Add new stuff to random bytes from time to time
+  {$ENDIF}
+
+  if (RAND_bytes(@fRealIV[0], 8) <> 1) then
+  begin
+    // fallback but deprecated in OpenSSL
+    if (RAND_pseudo_bytes(@fRealIV[0], 8) <> 1) then
+    begin
+      Debug(dpError, section, '[FiSH] Can not get random numbers for CBC encryption!');
+      exit;
+    end;
+  end;
+  // got an IV
+  move(fRealIV[0], fInBuf[0], 8);
+
+  // append unencrypted msg to buffer
+  move(dText[1], fInBuf[8], Length(dText));
+
+  {* encrypt... *}
+  fSuccess := BlowfishCipherWalk(@fCTX, @fInBuf[0], fInBufSize, eText);
+  if not fSuccess then
+    exit;
+
+  if (EVP_CIPHER_CTX_cleanup(@fCTX) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] EVP_CIPHER_CTX_cleanup failed for CBC encryption!');
+    // we can't do something...
+  end;
+
+  if (DoBase64Encode(eText, eTextBase64) < 1) then
+  begin
+    Debug(dpError, section, Format('[FiSH] Base64 Encode for %s failed!', [dText]));
+    exit;
+  end;
+
+  // append * to mark it as CBC message
+  Result := '*' + eTextBase64;
+end;
+
+function irc_cbc_decrypt(const netname, channel, eText: String): String;
+const
+  IV: array[0..7] of Byte = ($00, $00, $00, $00, $00, $00, $00, $00);
+var
+  bf: TIrcBlowkey;
+  bfKey: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+  fKeyLen: integer;
+  fCTX: EVP_CIPHER_CTX;
+  B64Text: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+  LengthB64Text: integer;
+  LengthB64TextMod: integer;
+  fBeenCut: boolean;
+  fSuccess: boolean;
+  DecryptedText: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+  i, j, fLength: integer;
+  fCleanedStr: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+begin
+  Result := eText;
+  bf := FindIrcBlowfish(netname, channel);
+  if ((bf = nil) or (bf.blowkey = '')) then
+  begin
+    Result := eText;
+    exit;
+  end;
+
+  if not bf.cbc then
+  begin
+    Debug(dpError, section, Format('[FiSH] Fishkey is for ECB but encrypted text is in CBC mode! %s@%s : %s', [channel, netname, eText]));
+    exit;
+  end;
+
+  // get used blowkey and length of it
+  bfKey := bf.blowkey;
+  fKeyLen := Length(bfKey);
+
+  {* init struct for decryption *}
+  EVP_CIPHER_CTX_init(@fCTX);
+  if (EVP_CipherInit_ex(@fCTX, EVP_bf_cbc(), nil, nil, nil, 0) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] First EVP_CipherInit_ex failed for CBC decryption!');
+    exit;
+  end;
+
+  {* set options *}
+  EVP_CIPHER_CTX_set_key_length(@fCTX, fKeyLen);
+  // MUST be the same setting used during encryption
+  EVP_CIPHER_CTX_set_padding(@fCTX, 0);
+
+  {* actually initialize session context *}
+  // 0 for decryption mode
+  if (EVP_CipherInit_ex(@fCTX, nil, nil, @bfKey[1], @IV[0], 0) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] Second EVP_CipherInit_ex failed for CBC decryption!');
+    exit;
+  end;
+
+  {* do base64 decryption *}
+  LengthB64Text := DoBase64Decode(eText, B64Text);
+  if (LengthB64Text < 1) then
+  begin
+    Debug(dpError, section, Format('[FiSH] Base64 Decode for %s failed!', [eText]));
+    exit;
+  end;
+
+  LengthB64TextMod := LengthB64Text mod 8;
+  fBeenCut := (LengthB64TextMod <> 0);
+  if fBeenCut then
+  begin
+    SetLength(B64Text, LengthB64Text - LengthB64TextMod);
+    LengthB64Text := Length(B64Text);
+  end;
+
+  {* decrypt... *}
+  fSuccess := BlowfishCipherWalk(@fCTX, @B64Text[1], LengthB64Text, DecryptedText);
+  if not fSuccess then
+    exit;
+
+  if (EVP_CIPHER_CTX_cleanup(@fCTX) <> 1) then
+  begin
+    Debug(dpError, section, '[FiSH] EVP_CIPHER_CTX_cleanup failed for CBC decryption!');
+    // we can't do something...
+  end;
+
+  // even if the decryption was not successful, there might be *some* data in
+  // the out buffer, so we should always do this
+  Delete(DecryptedText, 1, 8); // remove IV data
+
+  // remove bad \x00 and \x0d\x0a chars
+  // see https://github.com/flakes/mirc_fish_10/blob/master/fish_10/src/util.cpp#L211
+  SetLength(fCleanedStr, Length(DecryptedText));
+  fLength := 0;
+  for i := 1 to Length(DecryptedText) do
+  begin
+    j := Ord(DecryptedText[i]);
+    if not ((j = 0) or (j = 10) or (j = 13)) then
+    begin
+      Inc(fLength);
+      fCleanedStr[fLength] := DecryptedText[i];
+    end;
+  end;
+  SetLength(fCleanedStr, fLength);
+
+  {$IFDEF UNICODE}
+    Result := UTF8ToString(fCleanedStr);
+  {$ELSE}
+    Result := fCleanedStr;
+  {$ENDIF}
+end;
+
+
+{ ECB mode }
+function irc_ecb_encrypt(dText: String; BlowfishKeyData: TBlowfishData): {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+var
+  temp: {$IFDEF UNICODE}RawByteString{$ELSE}String{$ENDIF};
+  i: Integer;
+begin
   temp := '';
+  // encrypted text
+  Result := '';
+
+  // Each message is split into blocks of 8 bytes, encrypted individually.
+  // A block shorter than 8 bytes is padded with zeroes.
   if (length(dText) mod 8 > 0) then
     for i := 1 to 8 - (length(dText) mod 8) do
-      dText:= dText+ #0;
+      dText := dText + #0;
 
   SetLength(temp, 8);
 
   for i := 1 to length(dText) div 8 do
   begin
-    temp := Copy(dText, 1+(i-1)*8,8);
-    if bf.cbc then
-      BlowfishEncryptCBC(bf.KeyData, PAnsiChar(temp), PAnsiChar(temp))
-    else
-      BlowfishEncryptECB(bf.KeyData, PAnsiChar(temp), PAnsiChar(temp));
-
-    eText := eText + bytetoB64(temp);
+    temp := Copy(dText, 1 + (i - 1) * 8, 8);
+    BlowfishEncryptECB(BlowfishKeyData, PAnsiChar(temp), PAnsiChar(temp));
+    Result := Result + bytetoB64(temp);
   end;
-
-  if bf.cbc then
-    eText := '*' + eText + '==';
-
-  if include_ok then
-    Result := UTF8Decode('+OK ' + eText)
-  else
-    Result := UTF8Decode(eText);
 end;
-
-
 
 function irc_ecb_decrypt(const netname, channel, eText: String): String;
 var
@@ -260,6 +564,7 @@ var
   i : Integer;
   bf: TIrcBlowkey;
 begin
+  Result := eText;
   bf := FindIrcBlowfish(netname, channel);
   if ((bf = nil) or (bf.blowkey = '')) then
   begin
