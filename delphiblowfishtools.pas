7f7c87f12a350e08c04e765fd6082e39c4335038
Dreamboxx <Dreamboxx@protonmail.com>
2018-07-10 19:43:23 +0200

    style(delphiblowfishtools.pas): Cleanup

--- delphiblowfishtools.pas
+++ delphiblowfishtools.pas
@@ -1,74 +1,28 @@
 unit delphiblowfishtools;
 
 interface
+
 uses
   Sysutils;
 
-type
-{$IFDEF VER120}
-  dword= longword;
-{$ELSE}
-  dword= longint;
-{$ENDIF}
-
-(*
-function LRot16(X: word; c: integer): word; assembler;
-function RRot16(X: word; c: integer): word; assembler;
-function LRot32(X: dword; c: integer): dword; assembler;
-function RRot32(X: dword; c: integer): dword; assembler;
-*)
-procedure XorBlock(I1, I2, O1: PByteArray; Len: integer);
-procedure IncBlock(P: PByteArray; Len: integer);
+procedure XorBlock(I1, I2, O1: PByteArray; const Len: integer);
+procedure IncBlock(P: PByteArray; const Len: integer);
 
 implementation
 
-(*
-function LRot16(X: word; c: integer): word; assembler;
-asm
-  mov ecx,&c
-  mov ax,&X
-  rol ax,cl
-  mov &Result,ax
-end;
-
-function RRot16(X: word; c: integer): word; assembler;
-asm
-  mov ecx,&c
-  mov ax,&X
-  ror ax,cl
-  mov &Result,ax
-end;
-
-function LRot32(X: dword; c: integer): dword; assembler;
-asm
-  mov ecx,&c
-  mov eax,&X
-  rol eax,cl
-  mov &Result,eax
-end;
-
-function RRot32(X: dword; c: integer): dword; assembler;
-asm
-  mov ecx,&c
-  mov eax,&X
-  ror eax,cl
-  mov &Result,eax
-end;
-*)
-
-procedure XorBlock(I1, I2, O1: PByteArray; Len: integer);
+procedure XorBlock(I1, I2, O1: PByteArray; const Len: integer);
 var
   i: integer;
 begin
-  for i:= 0 to Len-1 do
-    O1[i]:= I1[i] xor I2[i];
+  for i := 0 to Len - 1 do
+    O1[i] := I1[i] xor I2[i];
 end;
 
-procedure IncBlock(P: PByteArray; Len: integer);
+procedure IncBlock(P: PByteArray; const Len: integer);
 begin
-  Inc(P[Len-1]);
-  if (P[Len-1]= 0) and (Len> 1) then
-    IncBlock(P,Len-1);
+  Inc(P[Len - 1]);
+  if (P[Len - 1] = 0) and (Len > 1) then
+    IncBlock(P, Len - 1);
 end;
 
 end.
