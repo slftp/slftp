d1034961d7afe5928bd545c0f5b87323e95fdd9d
slftp <slftp@slftp>
2018-07-12 20:34:23 +0200

    style(some): Formatting and const

--- slsignals.pas
+++ slsignals.pas
@@ -2,30 +2,31 @@ unit slsignals;
 
 interface
 
-procedure slSignal(signalNo: Longint; handler: Pointer);
+procedure slSignal(const signalNo: Longint; handler: Pointer);
 
 implementation
 
 uses
-{$IFDEF FPC}
-  BaseUnix
-{$ELSE}
-  Libc
-{$ENDIF}
-;
+  {$IFDEF FPC}
+    BaseUnix
+  {$ELSE}
+    Libc
+  {$ENDIF};
 
-procedure slSignal(signalNo: Longint; handler: Pointer);
+procedure slSignal(const signalNo: Longint; handler: Pointer);
 {$IFDEF FPC}
-var v: PSigActionRec;
+  var
+    v: PSigActionRec;
 {$ENDIF}
 begin
 {$IFDEF FPC}
   new(v);
-  v^.sa_Handler:=SigActionHandler(handler);
-  fillchar(v^.Sa_Mask,sizeof(v^.sa_mask),#0);
-  v^.Sa_Flags:=0;
-  {$ifdef Linux}               // Linux specific
-     v^.Sa_Restorer:=Nil;
+  v^.sa_Handler := SigActionHandler(handler);
+  fillchar(v^.Sa_Mask, sizeof(v^.sa_mask), #0);
+  v^.Sa_Flags := 0;
+  {$ifdef Linux}
+    // Linux specific
+     v^.Sa_Restorer := nil;
   {$endif}
 
   fpSigaction(signalNo, v, nil);
--- socks5.pas
+++ socks5.pas
@@ -2,34 +2,28 @@ unit socks5;
 
 interface
 
-uses sltcp, mslproxys;
+uses
+  sltcp, mslproxys;
 
 procedure Socks5Init;
 procedure SetupSocks5(c: TslTCPSocket; usesocks5: Boolean);
 
-procedure mSLSetupSocks5(proxyname: String; c: TslTCPSocket; usesocks5: Boolean);
+procedure mSLSetupSocks5(const proxyname: String; c: TslTCPSocket; usesocks5: Boolean);
 
 implementation
 
 uses configunit;
-const section = 'socks5';
 
-procedure mSLSetupSocks5(proxyname: String; c: TslTCPSocket; usesocks5: Boolean);
-var so5: TmSLSocks5;
+const
+  section = 'socks5';
+
+procedure Socks5Init;
 begin
-  so5 := FindProxyByName(proxyname);
-  if so5 = nil then
-  begin
-    c.socks5.enabled := False;
-  end
-  else
-  begin
-    c.socks5.enabled := so5.Enabled;
-    c.socks5.Host := so5.host;
-    c.socks5.Port := so5.port;
-    c.socks5.Username := so5.username;
-    c.socks5.Password := so5.password;
-  end;
+  slDefaultSocks5.Enabled := config.ReadBool(section, 'enabled', False);
+  slDefaultSocks5.Host := config.ReadString(section, 'host', '127.0.0.1');
+  slDefaultSocks5.Port := config.ReadInteger(section, 'port', 11101);
+  slDefaultSocks5.Username := config.ReadString(section, 'username', 'user');
+  slDefaultSocks5.Password := config.ReadString(section, 'password', 'pw');
 end;
 
 procedure SetupSocks5(c: TslTCPSocket; usesocks5: Boolean);
@@ -46,14 +40,23 @@ begin
     c.socks5.enabled := True;
 end;
 
-procedure Socks5Init;
+procedure mSLSetupSocks5(const proxyname: String; c: TslTCPSocket; usesocks5: Boolean);
+var
+  so5: TmSLSocks5;
 begin
-  slDefaultSocks5.Enabled := True;
-  slDefaultSocks5.Enabled := config.ReadBool(section, 'enabled', False);
-  slDefaultSocks5.Host := config.ReadString(section, 'host', '');
-  slDefaultSocks5.Port := config.ReadInteger(section, 'port', 0);
-  slDefaultSocks5.Username := config.ReadString(section, 'username', '');
-  slDefaultSocks5.Password := config.ReadString(section, 'password', '');
+  so5 := FindProxyByName(proxyname);
+  if so5 = nil then
+  begin
+    c.socks5.enabled := False;
+  end
+  else
+  begin
+    c.socks5.enabled := so5.Enabled;
+    c.socks5.Host := so5.host;
+    c.socks5.Port := so5.port;
+    c.socks5.Username := so5.username;
+    c.socks5.Password := so5.password;
+  end;
 end;
 
 end.
