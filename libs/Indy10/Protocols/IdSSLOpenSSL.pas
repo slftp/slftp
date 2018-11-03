fa1804531382f669ad57d785a2661a1304b85479
DEADC0DE <Dreamboxx@protonmail.com>
2018-11-03 14:26:11 +0100

    update(Indy): Update to r5482

--- libs/Indy10/Protocols/IdSSLOpenSSL.pas
+++ libs/Indy10/Protocols/IdSSLOpenSSL.pas
@@ -2769,10 +2769,18 @@ procedure TIdSSLIOHandlerSocketOpenSSL.ConnectClient;
 var
   LPassThrough: Boolean;
 begin
+  // RLebeau: initialize OpenSSL before connecting the socket...
+  try
+    Init;
+  except
+    on EIdOSSLCouldNotLoadSSLLibrary do begin
+      if not PassThrough then raise;
+    end;
+  end;
   // RLebeau 1/11/07: In case a proxy is being used, pass through
   // any data from the base class unencrypted when setting up that
   // connection.  We should do this anyway since SSL hasn't been
-  // initialized yet!
+  // negotiated yet!
   LPassThrough := fPassThrough;
   fPassThrough := True;
   try
@@ -2788,13 +2796,6 @@ end;
 
 procedure TIdSSLIOHandlerSocketOpenSSL.StartSSL;
 begin
-  try
-    Init;
-  except
-    on EIdOSSLCouldNotLoadSSLLibrary do begin
-      if not PassThrough then raise;
-    end;
-  end;
   if not PassThrough then begin
     OpenEncodedConnection;
   end;
@@ -2866,6 +2867,14 @@ procedure TIdSSLIOHandlerSocketOpenSSL.AfterAccept;
 begin
   try
     inherited AfterAccept;
+    // RLebeau: initialize OpenSSL after accepting a client socket...
+    try
+      Init;
+    except
+      on EIdOSSLCouldNotLoadSSLLibrary do begin
+        if not PassThrough then raise;
+      end;
+    end;
     StartSSL;
   except
     Close;
@@ -2946,6 +2955,8 @@ var
   {$ENDIF}
   LMode: TIdSSLMode;
   LHost: string;
+
+  // TODO: move the following to TIdSSLIOHandlerSocketBase...
 
   function GetURIHost: string;
   var
@@ -3323,6 +3334,8 @@ an invalid MAC when doing SSL.}
   if RootCertFile <> '' then begin    {Do not Localize}
     SSL_CTX_set_client_CA_list(fContext, IndySSL_load_client_CA_file(RootCertFile));
   end
+
+  // TODO: provide an event so users can apply their own settings as needed...
 end;
 
 procedure TIdSSLContext.SetVerifyMode(Mode: TIdSSLVerifyModeSet; CheckRoutine: Boolean);
@@ -3775,7 +3788,7 @@ var
   ret, err: Integer;
 begin
   repeat
-    ret := SSL_read(fSSL, @ABuffer[0], Length(ABuffer));
+    ret := SSL_read(fSSL, PByte(ABuffer), Length(ABuffer));
     if ret > 0 then begin
       Result := ret;
       Exit;
