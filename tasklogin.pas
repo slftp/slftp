aa2b2b4798aa890b36063c31ccd2d33763565000
Dreamboxx <Dreamboxx@protonmail.com>
2019-01-19 23:08:27 +0100

    refactor(TSite): Add Properties for config values
    - added some properties to TSite for autobnctest, autonuke, autoindex
    and autodirlist + use them in other units
    - some small changes in tasklogin to remove 'GOTO' construct

--- tasklogin.pas
+++ tasklogin.pas
@@ -6,14 +6,14 @@ uses tasksunit;
 
 type
   TLoginTask = class(TTask)
-    private
-      kill: Boolean;
-    public
-      noannounce: Boolean;
-      readd: Boolean; // used for autobnctest
-      constructor Create(const netname, channel, site: String; kill: Boolean; readd: Boolean);
-      function Execute(slot: Pointer): Boolean; override;
-      function Name: String; override;
+  private
+    kill: Boolean;
+  public
+    noannounce: Boolean;
+    readd: Boolean; //< @true if called from autobnctest, @false otherwise
+    constructor Create(const netname, channel, site: String; kill: Boolean; readd: Boolean);
+    function Execute(slot: Pointer): Boolean; override;
+    function Name: String; override;
   end;
 
 implementation
@@ -28,27 +28,27 @@ const
 
 constructor TLoginTask.Create(const netname, channel, site: String; kill: Boolean; readd: Boolean);
 begin
+  inherited Create(netname, channel, site);
   self.kill := kill;
   self.readd := readd;
-  inherited Create(netname, channel, site);
 end;
 
 function TLoginTask.Execute(slot: Pointer): Boolean;
-label
-  autobnctest;
 var
   s: TSiteSlot;
   i: Integer;
   l: TLoginTask;
 begin
   Result := False;
+  Debug(dpSpam, section, '-->' + Name);
   s := slot;
-  debugunit.Debug(dpSpam, section, '-->' + Name);
+  i := s.site.AutoBncTestInterval;
 
+  // readd is only true if called by autobnctest
   if readd then
   begin
-    // readd is for autobnctest - if autobnctest for site is disabled we don't need to go further
-    if s.RCInteger('autobnctest', 0) = 0 then
+    // if autobnctest for site is disabled we don't need to go further
+    if i = 0 then
     begin
       ready := True;
       Result := True;
@@ -56,59 +56,40 @@ begin
     end;
   end;
 
-  if ((s.site.working = sstUp) and (readd)) then
+  if not ((s.site.working = sstUp) and (readd)) then
   begin
-    // site is up, we have to try to login
-    goto autobnctest;
-  end;
-
-  try
+    // site is not up, we have to try to login
     if ((not readd) or (not s.site.markeddown)) then
     begin
       s.Quit;
       Result := s.ReLogin(1, kill, section);
     end;
-  except
-    on e: Exception do
+
+    if s.Status = ssOnline then
     begin
-      Debug(dpError, section, Format('[EXCEPTION] TLoginTask.s.ReLogin: %s', [e.Message]));
+      // slot is online
+      announce := Format('<b>%s</b>: %s', [s.site.Name, s.bnc]);
     end;
   end;
 
-  if s.Status = ssOnline then
-  begin
-    announce := Format('<b>%s</b>: %s',[s.site.name, s.bnc]);
-  end;
-
-autobnctest:
   if readd then
   begin
-{
- we check this above and if s.RCInteger('autobnctest', 0) = 0 we exit!
- if autobnctest for site is disabled we don't need to go further
-       -------------------------------------------------
-  generates an high CPU load when a sites has autobnctest value = 0 
-}
-
-    i := s.RCInteger('autobnctest', 0);
-    if i > 0 then
-    begin
-      try
-        l := TLoginTask.Create(netname, channel, site1, kill, readd);
-        l.startat := IncSecond(Now, i);
-        l.dontremove := True;
-        AddTask(l);
-      except
-        on e: Exception do
-        begin
-          Debug(dpError, section, Format('[EXCEPTION] TLoginTask.Execute.AddTask: %s', [e.Message]));
-        end;
+    try
+      l := TLoginTask.Create(netname, channel, site1, kill, readd);
+      l.startat := IncSecond(Now, i);
+      l.dontremove := True;
+      AddTask(l);
+    except
+      on e: Exception do
+      begin
+        Debug(dpError, section, Format('[EXCEPTION] TLoginTask.Execute.AddTask: %s', [e.Message]));
       end;
     end;
   end;
 
-  debugunit.Debug(dpSpam, section, '<--' + Name);
   ready := True;
+  Debug(dpSpam, section, '<--' + Name);
+  Result := True;
 end;
 
 function TLoginTask.Name: String;
@@ -120,7 +101,7 @@ begin
       Result := 'AUTO';
     end;
 
-    Result := Result + Format('LOGIN <b>%s</b> %s', [site1, ScheduleText]);
+    Result := Result + Format('LOGIN %s %s', [site1, ScheduleText]);
   except
     Result := 'LOGIN';
   end;
