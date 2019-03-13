711775429f3880a9c90459ba2fa7af791e93bf98
DEADC0DE <Dreamboxx@protonmail.com>
2019-03-13 19:07:22 +0100

    Update(make.bat): Formatting, Capture errorlevel correctly

--- make.bat
+++ make.bat
@@ -8,6 +8,7 @@ set CC_EXTRAS=-NSWinapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Wi
 set CFLAGS=-B -$O+,C+,D-,L-
 set CDBFLAGS=-B -$O+,C+,D+,L+
 set CINCLUDES=-Uirccommands -Ulibs/FastMM4 -Ulibs/BeRoHighResolutionTimer -Ulibs/FLRE -Ulibs/rcmdline -Ulibs/DFFLibV15_UIntList -Ulibs/lkJSON -Ulibs/TRegExpr -Ulibs/pasmp -Ulibs/Indy10/Core -Ulibs/Indy10/Protocols -Ulibs/Indy10/System -Ulibs/LibTar -Ulibs/mORMot -Ulibs/ZeosLib -Ulibs/ZeosLib/core -Ulibs/ZeosLib/dbc -Ulibs/ZeosLib/parsesql -Ulibs/ZeosLib/plain
+set UnitTestAppName=tests\slftpUnitTests.exe
 
 REM
 REM default: 64bit (2018!)
@@ -28,79 +29,87 @@ goto :error
 
 :slftp_32
 del /q *.exe *.dcu
-echo Compiling Win32 RELEASE slftp.exe
+echo --- Compiling Win32 RELEASE slftp.exe ---
 echo "%CC_32%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 "%CC_32%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason given is %errorlevel%
    exit /b %errorlevel%
 )
 goto :eof
 
 :slftp_64
 del /q *.exe *.dcu
-echo Compiling Win64 RELEASE slftp.exe
+echo --- Compiling Win64 RELEASE slftp.exe ---
 echo "%CC_64%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 "%CC_64%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason given is %errorlevel%
    exit /b %errorlevel%
 )
 goto :eof
 
 :slftp_32_debug
 del /q *.exe *.dcu
-echo Compiling Win32 DEBUG slftp.exe
+echo --- Compiling Win32 DEBUG slftp.exe ---
 echo "%CC_32%" %CDBFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 "%CC_32%" %CDBFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason given is %errorlevel%
    exit /b %errorlevel%
 )
 goto :eof
 
 :slftp_64_debug
 del /q *.exe *.dcu
-echo Compiling Win64 DEBUG slftp.exe
+echo --- Compiling Win64 DEBUG slftp.exe ---
 echo "%CC_64%" %CDBFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 "%CC_64%" %CDBFLAGS% %CC_EXTRAS% %CINCLUDES% slftp.dpr
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason given is %errorlevel%
    exit /b %errorlevel%
 )
 goto :eof
 
 :clean
-echo Cleaning files
+echo --- Cleaning files ---
 del /q *.exe *.dcu
 goto :eof
 
 :test_32
 del /q *.exe *.dcu
-echo Testing Win64
+echo -- Testing Win32 ---
 echo "%CC_32%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% tests\slftpUnitTests.dpr
 "%CC_32%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% tests\slftpUnitTests.dpr
-"tests\slftpUnitTests.exe"
-cd tests
-del /q *.exe *.dcu
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason for compiling tests is %errorlevel%
    exit /b %errorlevel%
 )
+"%UnitTestAppName%"
+if errorlevel 1 (
+   echo Failure reason for running tests is %errorlevel%
+   exit /b %errorlevel%
+)
+cd tests
+del /q *.exe *.dcu
 goto :eof
 
 :test_64
 del /q *.exe *.dcu
-echo Testing Win64
+echo -- Testing Win64 ---
 echo "%CC_64%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% tests\slftpUnitTests.dpr
 "%CC_64%" %CFLAGS% %CC_EXTRAS% %CINCLUDES% tests\slftpUnitTests.dpr
-"tests\slftpUnitTests.exe"
-cd tests
-del /q *.exe *.dcu
 if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
+   echo Failure reason for compiling tests is %errorlevel%
    exit /b %errorlevel%
 )
+"%UnitTestAppName%"
+if errorlevel 1 (
+   echo Failure reason for running tests is %errorlevel%
+   exit /b %errorlevel%
+)
+cd tests
+del /q *.exe *.dcu
 goto :eof
 
 :error
