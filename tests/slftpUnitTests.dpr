7e82ab6bf0873f2c401a8c1bbccce0fbd929b0b2
slftp <slftp@slftp>
2019-03-13 22:19:43 +0100

    Update(UnitTests): DUnitX for Delphi
    Use newer DUnitX for Delphi unit testing which will hopefully work
    soon with FPC if they added support for attributes 
    but DUnitX is compatible with DUnit2
    
    Files for DUnitX will be checked in later
    (normally installed by Delphi afaik)

--- tests/slftpUnitTests.dpr
+++ tests/slftpUnitTests.dpr
@@ -11,37 +11,67 @@ program slftpUnitTests;
 {$endif}
 
 {$IF Defined(TextRunner)}
-  {$IFDEF WINDOWS}
+  {$IFDEF MSWINDOWS}
     {$APPTYPE CONSOLE}
   {$ENDIF}
 {$ELSE}
-  {$DEFINE GUIRunner}
-  {$IFDEF WINDOWS}
+  {$IFDEF MSWINDOWS}
     {$APPTYPE GUI}
   {$ENDIF}
 {$ENDIF}
 
 uses
   FastMM4,
+  DUnitX.TestFramework,
   {$IFDEF TextRunner}
-    TextTestRunner,
+    DUnitX.Loggers.Console,
   {$ELSE}
-    GUITestRunner,
+    FMX.Forms,
+    DUnitX.Loggers.GUIX,
   {$ENDIF}
   Classes, SysUtils,
   // add all test units below
-  Base64OpenSSLTests,
+  //Base64OpenSSLTests, {* TODO: does not compile *}
   mystringsTests;
 
 // allow more user mode address space
 {$SetPEFlags $20}
 
+var
+  runner : ITestRunner;
+  results : IRunResults;
+  logger : ITestLogger;
+
 begin
   // run all registered tests
-{$IFDEF TextRunner}
-  // halt on error, means exit code <> 0
-  RunRegisteredTests(rxbHaltOnFailures);
-{$ELSE}
-  RunRegisteredTests;
-{$ENDIF}
+  {$IFDEF TextRunner}
+    try
+      // check for command line commands
+      TDUnitX.CheckCommandLine;
+      // create runner
+      runner := TDUnitX.CreateRunner;
+      runner.UseRTTI := True;
+      // tell the runner how we will log things
+      logger := TDUnitXConsoleLogger.Create(true);
+      runner.AddLogger(logger);
+
+      // run tests
+      results := runner.Execute;
+      if not results.AllPassed then
+        System.ExitCode := EXIT_ERRORS;
+
+      if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
+      begin
+        System.Write('Done. press <Enter> key to quit.');
+        System.Readln;
+      end;
+    except
+      on E: Exception do
+        System.Writeln(E.ClassName, ': ', E.Message);
+    end;
+  {$ELSE}
+    Application.Initialize;
+    Application.CreateForm(TGUIXTestRunner, GUIXTestRunner);
+    Application.Run;
+  {$ENDIF}
 end.
