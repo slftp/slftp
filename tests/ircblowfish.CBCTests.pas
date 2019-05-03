82b7d8b28d55b2173b9c651727df7876a4da1016
slftp <slftp@slftp>
2019-05-03 22:19:56 +0200

    fix(WindowsTests): Add missing unit

--- tests/ircblowfish.CBCTests.pas
+++ tests/ircblowfish.CBCTests.pas
@@ -7,7 +7,7 @@ uses
   {$IFDEF FPC}
     TestFramework;
   {$ELSE}
-    DUnitX.TestFramework, DUnitX.DUnitCompatibility;
+    DUnitX.TestFramework, DUnitX.DUnitCompatibility, DUnitX.Assert;
   {$ENDIF}
 
 type
