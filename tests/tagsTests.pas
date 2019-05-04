e04c36681bb743a6b795418656911c69ca1bbb8b
slftp <slftp@slftp>
2019-05-04 23:25:37 +0200

    refactor(tagsTests): Only use realistic (in-)complete lines

--- tests/tagsTests.pas
+++ tests/tagsTests.pas
@@ -24,16 +24,25 @@ uses
 
 procedure TTestTags.TestCheckStandardPercentDir;
 begin
+  {
+    expected to fail because of function expects i > 4 for i := pos('% complete')
   CheckEquals(1, CheckStandardPercentDir('100% complete'));
-  CheckEquals(1, CheckStandardPercentDir('[##############] -  100% Complete - [xxx]'));
   CheckEquals(-1, CheckStandardPercentDir('99% complete'));
   CheckEquals(-1, CheckStandardPercentDir('35% complete'));
+  }
+  CheckEquals(1, CheckStandardPercentDir('[##############] -  100% Complete - [xxx]'));
+  CheckEquals(1, CheckStandardPercentDir('[ 23 of 23 files = 100% complete of 335.1MB]'));
   CheckEquals(-1, CheckStandardPercentDir('[##::::::::::::] -  18% Complete - [xxx]'));
+  CheckEquals(-1, CheckStandardPercentDir('[8 of 10 files = 80% complete at 366.8MB]'));
+  CheckEquals(-1, CheckStandardPercentDir('[::::::::::::::] -   0% Complete - [x]'));
   CheckEquals(0, CheckStandardPercentDir('[ 73%]-[##########::::]-[729mb]'));
   CheckEquals(0, CheckStandardPercentDir('<<SL>> 11M 1F of House from 2017 COMPLETE'));
   CheckEquals(0, CheckStandardPercentDir('-> 8128M 86F - DONE <-'));
-  CheckEquals(0, CheckStandardPercentDir('done'));
-  CheckEquals(0, CheckStandardPercentDir('35 files complete'));
+  CheckEquals(0, CheckStandardPercentDir('[>>>>>>>>>>>   ] -  83DONE'));
+  CheckEquals(0, CheckStandardPercentDir('[Completed! - 1252M 27F]'));
+  CheckEquals(0, CheckStandardPercentDir('( 122M 13F - COMPLETE )'));
+  CheckEquals(0, CheckStandardPercentDir('[x] - ( 135M 2F - COMPLETE - Dance Hall 1990 ) - [x]'));
+  CheckEquals(0, CheckStandardPercentDir('[x] - ( 145M 31F - COMPLETE ) - [x]'));
 end;
 
 initialization
