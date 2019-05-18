cbe8d86e5263011c0d34c949b301ae495fda5b6e
slftp <slftp@slftp>
2019-05-18 20:00:11 +0200

    refactor(taskautodirlist): add some comments, change some names

--- taskautodirlist.pas
+++ taskautodirlist.pas
@@ -26,9 +26,9 @@ const
 type
   TReqFillerThread = class(TThread)
   private
-    p: TPazo;
-    secdir: String;
-    rlsname: String;
+    p: TPazo; //< associated pazo element with the sites and releases
+    secdir: String; //< directory of the request section on site
+    rlsname: String; //< name of the release being filled in this request
   public
     constructor Create(p: Tpazo; const secdir, rlsname: String);
     procedure Execute; override;
@@ -38,7 +38,7 @@ type
 
 procedure TAutoDirlistTask.ProcessRequest(slot: Pointer; const secdir, reqdir, releasename: String);
 var
-  x: TStringList;
+  x: TStringList; //< result list for the indexer query
   i, db: Integer;
   sitename: String;
   p: TPazo;
@@ -47,11 +47,11 @@ var
   rls: TRelease;
   s: TSiteSlot;
   ss: String;
-  datum: String;
-  maindir: String;
-  releasenametofind: String;
-  notdown: Boolean;
-  prestatus: Boolean;
+  datum: String; //< helper var to remove prefixed dates in yyyy-mm-dd_ shape (or similar)
+  maindir: String; //< full path of reqdir on site
+  releasenametofind: String; //< actual releasename, with possible date prefix removed
+  notdown: Boolean; //< @true if site is not down, @false otherwise
+  prestatus: Boolean; //< @true if source site for the request, @false if destination site
   site: TSite;
   pdt: TPazoDirlistTask;
 begin
@@ -86,7 +86,7 @@ begin
       sitename := Fetch(ss, '-', True, False);
       if sitename = site1 then
       begin
-        // lolka fel van toltve helyben.  -- Lolka is charged on site.
+        // Requested release is already indexed on this site, not going to send it
         ss := x.Values[x.Names[i]];
         ss := ReplaceText(ss, '/', '_');
 
@@ -104,7 +104,6 @@ begin
       site := FindSiteByName(netname, sitename);
       notdown := ((site <> nil) and (site.working <> sstDown));
 
-
       if ((notdown) and (
         (site.isRouteableTo(site1)) or not
         (config.ReadBool(rsections, 'only_use_routable_sites_on_reqfill', False))
@@ -119,7 +118,7 @@ begin
 
     if db > 0 then
     begin
-      // jo, addolunk racetaskot dirlistekkel meg minden   ~~ jo, addolunk racetaskot dirlistekkel all
+      // Found at least one site that has the release, issue dirlists for each one and create pazo to send it to destination
       ss := x.Names[0];
       Fetch(ss, '-', True, False);
 
@@ -140,7 +139,12 @@ begin
       for i := 0 to p.sites.Count - 1 do
       begin
         try
-          // Treat source sites for filling as presites, destination site is a normal race destination
+          (*
+            Treat source sites for filling as presites, destination site is a normal race destination
+            this leads to only dirlisting the source sites once, as they're unlikely to change in content
+            but we will dirlist the destination site unless the release was sent completely or any of the
+            other conditions for ending a dirlist kick in
+          *)
           if (i = 0) then
             prestatus := False
           else
@@ -172,9 +176,9 @@ var
   de: TDirListEntry;
   reqrgx: TRegExpr;
 
-  procedure UjraAddolas;
+  procedure RescheduleTask;
   begin
-    // megnezzuk, kell e meg a taszk -- We look at whether it should be the task
+    // Check autodirlist interval
     i := s.RCInteger('autodirlist', 0);
     if i > 0 then
     begin
@@ -187,7 +191,7 @@ var
       except
         on e: Exception do
         begin
-          Debug(dpError, section, Format('[EXCEPTION] TAutoDirlistTask.Execute AddTask: %s', [e.Message]));
+          Debug(dpError, section, Format('[EXCEPTION] TAutoDirlistTask.Execute RescheduleTask: %s', [e.Message]));
         end;
       end;
     end;
@@ -198,7 +202,7 @@ begin
   s := slot;
   debugunit.Debug(dpMessage, rsections, Name);
 
-  // megnezzuk, kell e meg a taszk -- We look at whether it should be the task
+  // Check autodirlist interval, whether autodirlist is still enabled
   if s.RCInteger('autodirlist', 0) = 0 then
   begin
     ready := True;
@@ -208,7 +212,7 @@ begin
 
   if s.site.working = sstDown then
   begin
-    ujraaddolas();
+    RescheduleTask();
     readyerror := True;
     exit;
   end;
@@ -217,7 +221,7 @@ begin
   begin
     if (not s.ReLogin(1)) then
     begin
-      ujraaddolas();
+      RescheduleTask();
       readyerror := True;
       exit;
     end;
@@ -289,7 +293,7 @@ begin
     end;
   end;
 
-  ujraaddolas();
+  RescheduleTask();
 
   Result := True;
   ready := True;
