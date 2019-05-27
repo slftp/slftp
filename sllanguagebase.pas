739d68ff79124033c8f26878de8e808ce8b89984
DEADC0DE <Dreamboxx@protonmail.com>
2019-05-27 22:58:07 +0200

    refactor(sllanguagebase): Overhaul complete code + optimized for speed + Tests
    - removed unused functions
    - remove code duplication due to new SLLoadMP3LanguagesFromIniFile procedure
    - uses generic TObjectList for type safety and a modern code base
    - functions specialized for only one case (lang vs mp3lang)
    - first step to remove old language base (related to #241)
    - comments
    - unit tests for functions FindLanguageOnDirectory and FindMusicLanguageOnDirectory
    
    FindLanguageOnDirectory:
    - adds support for non regex matching (regex matching is only needed if Expression contains '|')
    - **using regex matching only when needed is about 4 to 115 times faster**
    FindMusicLanguageOnDirectory:
    - mp3languages list does not support regex expressions at all (no need to use regex matching)
    - speedup by a factor of about 45 to 250

--- sllanguagebase.pas
+++ sllanguagebase.pas
@@ -2,256 +2,135 @@ unit sllanguagebase;
 
 interface
 
-uses SysUtils, Classes, Contnrs, IniFiles, configunit;
+uses
+  SysUtils, Classes, Contnrs, Generics.Collections;
 
 type
+ { @abstract(Class for language and its expression(s) in scene tagging) }
   TSLLanguages = class
   private
-    inisection, fexpression, flang: String;
-    ffileindex: integer;
+    FLanguage: String; //< language (text before equality sign in file)
+    FExpression: String; //< lowercased expression(s) which are used in tagging for these language (text after equality sign in file)
+    FExpressionLength: Integer; //< String length of @link(FExpression)
+    FFileIndex: integer; //< row number from file with the infos
+    FIsRegexExpression: Boolean; //< if @true it uses slower regex matching, otherwise it uses fast RTL Pos() matching
   public
-    constructor Create(IniSection, Language, Expression: String; fileindex: integer);
-    procedure Rehash;
-    property Language: String Read flang;
-    property Expression: String Read fexpression;
+    { Just a helper function to create a new @link(TSLLanguages) class
+      @param(aLanguage language)
+      @param(aExpression expression(s) for these language (automatically lowercased))
+      @param(aFileIndex row number in file)
+      @param(aIsRegexExpression set to @true if Language should be found via regex matching, otherwise set it to @false ) }
+    constructor Create(const aLanguage, aExpression: String; const aFileIndex: integer; const aIsRegexExpression: Boolean);
+    property Language: String read FLanguage; //< language
+    property Expression: String read FExpression; //< lowercased expression(s) which are used in tagging for these language
+    property ExpressionLength: Integer read FExpressionLength; //< String length of @link(Expression)
+    property NeedsRegexMatching: Boolean read FIsRegexExpression; //< @true if @link(Expression) is a regex pattern, @false otherwise
   end;
 
+{ Just a helper function to initialize @link(sllanguages) with languages from slftp.languagebase and @link(slmp3languages) with infos from @link(kb.mp3languages) }
+procedure SLLanguagesInit;
+{ Just a helper function to free @link(sllanguages) and @link(slmp3languages) }
+procedure SLLanguagesUninit;
+{ Reads mp3languages from kb section in slftp inifile and stores them uppercased in @link(aStringList), @italic(list will be automatically cleared)
+  @param(aStringList Stringlist which should be used for storing MP3 languages) }
+procedure SLLoadMP3LanguagesFromIniFile(var aStringList: TStringList);
+{ Loads current values from files to @link(sllanguages) and @link(slmp3languages)
+  @returns(Count of loaded languages and mp3 languages) }
 function SLLanguagesReload: String;
-
-procedure SLLanguagesRehash(out status: String);
-
-procedure SLLanguages_Init;
-procedure SLLanguages_Uninit;
-
-function SLLanguagesFindLanguage(Text: String; mp3language: boolean = False): String; overload;
-function SLLanguagesFindLanguage(Text: TStringList; mp3language: boolean = False): String; overload;
-
-function FindSLLanguage(Language: String; mp3language: boolean = False): TSLLanguages;
-
-function FindLanguageOnDirectory(Text: String; mp3language: boolean = False): String;
+{ Iterates through @link(sllanguage) and searches with @link(TSLLanguages.Expression) in @link(aRlsname)
+  @param(aRlsname string in which it searches for the language)
+  @returns(@link(TSLLanguages.Language) string if found, otherwise default 'English') }
+function FindLanguageOnDirectory(const aRlsname: String): String;
+{ Iterates through @link(slmp3languages) and searches with each string of it in @link(aRlsname)
+  @param(aRlsname string in which it searches for the language)
+  @returns(Language string of matched @link(slmp3languages) if found, otherwise default 'EN') }
+function FindMusicLanguageOnDirectory(const aRlsname: String): String;
 
 var
-  sllanguages:    TObjectList;
-  slmp3languages: TStringList;
-  sllanguagefile: TIniFile;
+  sllanguages: TObjectList<TSLLanguages>; //< Holds @link(TSLLanguages) objects with the expressions/language from slftp.languagebase
+  slmp3languages: TStringList; //< Holds mp3 languages from kb section from inifile (same as @link(kb.mp3languages))
 
 implementation
 
-uses mystrings, debugunit, Regexpr;
+uses
+  configunit, mystrings, debugunit, Regexpr, StrUtils;
 
 const
   rsections = 'sllanguagebase';
 
-{TSLLanguages}
-
-constructor TSLLanguages.Create(IniSection: String; Language: String; Expression: String; fileindex: integer);
-begin
-  inisection := IniSection;
-  ffileindex := fileindex;
-  flang := Language;
-  fexpression := Expression;
-end;
-
-procedure TSLLanguages.Rehash;
-begin
-  fexpression := sllanguagefile.ReadString(inisection, flang, '');
-end;
-
-{SLLanguages Find Utils}
-
-function FindSLLanguage(Language: String; mp3language: boolean = False): TSLLanguages;
+{ Reads languages from slftp.languagebase and stores them in @link(aObjectList), @italic(list will be automatically cleared)
+  @param(aObjectList ObjectList which should be used for storing languages) }
+procedure _ReadLanguagesFromIniFile(var aObjectList: TObjectList<TSLLanguages>);
 var
-  i: integer;
-begin
-  Result := nil;
-
-  for i := 0 to sllanguages.Count - 1 do
-    if TSLLanguages(sllanguages.Items[i]).Language = Language then
-    begin
-      Result := TSLLanguages(sllanguages.Items[i]);
-      break;
-    end;
-end;
-
-function SLLanguagesFindLanguage(Text: String; mp3language: boolean = False): String;
-var
-  i: integer;
-  lrx: TRegexpr;
+  i: Integer;
+  y: TStringList;
   sllang: TSLLanguages;
+  fIsRegexPattern: Boolean;
 begin
-  lrx := TRegexpr.Create;
-  try
-    lrx.ModifierI := True;
+  aObjectList.Clear;
 
-    if mp3language then
-    begin
-      Result := 'EN';
-      for i := 0 to slmp3languages.Count - 1 do
-      begin
-        lrx.Expression := '[\-](' + slmp3languages[i] + ')[\-]';
-        if lrx.Exec(text) then
-        begin
-          Result := slmp3languages[i];
-          break;
-        end;
-      end;
-    end
-    else
-    begin
-      Result := 'English';
-      for i := 0 to sllanguages.Count - 1 do
-      begin
-        sllang := TSLLanguages(sllanguages.Items[i]);
-        lrx.Expression := '[\.\-\_](' + sllang.Expression + ')[\.\-\_]';
-        if lrx.Exec(Text) then
-        begin
-          Result := sllang.Language;
-          break;
-        end;
-      end;
-    end;
-
-  finally
-    lrx.Free;
-  end;
-end;
-
-function SLLanguagesFindLanguage(Text: TStringList; mp3language: boolean = False): String;
-var
-  i: integer;
-  s: String;
-begin
-  if mp3language then
-  begin
-    Result := 'EN';
-  end
-  else
-  begin
-    Result := 'English';
-  end;
-
-  for i := 0 to Text.Count - 1 do
-  begin
-    s := '';
-    s := SLLanguagesFindLanguage(Text.Strings[i], mp3language);
-    if s <> '' then
-    begin
-      Result := s;
-      break;
-    end;
-  end;
-end;
-
-
-function FindLanguageOnDirectory(Text: String; mp3language: boolean = False): String;
-var
-  i: integer;
-  lrx: TRegexpr;
-  sllang: TSLLanguages;
-begin
-  lrx := TRegexpr.Create;
-  try
-    lrx.ModifierI := True;
-
-    if mp3language then
-    begin
-      Result := 'EN';
-      for i := 0 to slmp3languages.Count - 1 do
-      begin
-        lrx.Expression := '[\-](' + slmp3languages[i] + ')[\-]';
-        if lrx.Exec(text) then
-        begin
-          Result := slmp3languages[i];
-          break;
-        end;
-      end;
-    end
-    else
-    begin
-      Result := 'English';
-      for i := 0 to sllanguages.Count - 1 do
-      begin
-        sllang := TSLLanguages(sllanguages.Items[i]);
-        lrx.Expression := '[\.\-\_](' + sllang.Expression + ')[\.\-\_]';
-        if lrx.Exec(Text) then
-        begin
-          Result := sllang.Language;
-          break;
-        end;
-      end;
-
-     end;
-  finally
-    lrx.Free;
-  end;
-end;
-
-
-{SLLanguages Common Utils}
-procedure renameFile;
-var
-  y: TStringlist;
-begin
   y := TStringList.Create;
   try
-    y.LoadFromFile(ExtractFilePath(ParamStr(0)) + 'languagebase.slftp');
-    y.SaveToFile(ExtractFilePath(ParamStr(0)) + 'slftp.languagebase');
-    {$IFDEF MSWINDOWS}
-      DeleteFile(PAnsiChar(ExtractFilePath(ParamStr(0)) + 'languagebase.slftp'));
-    {$ELSE}
-      DeleteFile(ExtractFilePath(ParamStr(0)) + 'languagebase.slftp');
-    {$ENDIF}
+    y.LoadFromFile(ExtractFilePath(ParamStr(0)) + 'slftp.languagebase');
+    for i := 0 to y.Count - 1 do
+    begin
+      if ((y.Strings[i][1] = '[') and (y.Strings[i][Length(y.Strings[i])] = ']')) then
+        Continue;
+
+      fIsRegexPattern := {$IFDEF UNICODE}ContainsText{$ELSE}AnsiContainsText{$ENDIF}(SubString(y.Strings[i], '=', 2), '|');
+
+      sllang := TSLLanguages.Create(SubString(y.Strings[i], '=', 1), SubString(y.Strings[i], '=', 2), i, fIsRegexPattern);
+      aObjectList.Add(sllang);
+    end;
   finally
     y.Free;
   end;
 end;
 
-procedure SLLanguages_Init;
+{ TSLLanguages }
+
+constructor TSLLanguages.Create(const aLanguage, aExpression: String; const aFileIndex: integer; const aIsRegexExpression: Boolean);
+begin
+  FLanguage := aLanguage;
+  FExpression := LowerCase(aExpression);
+  FExpressionLength := Length(aExpression);
+  FFileIndex := aFileIndex;
+  FIsRegexExpression := aIsRegexExpression;
+end;
+
+procedure SLLanguagesInit;
 var
   i: integer;
   y: TStringList;
 begin
-  Debug(dpSpam, rsections, 'Loading Language Base ->');
+  Debug(dpSpam, rsections, '-> Loading Language Base');
 
-  sllanguages := TObjectList.Create;
+  sllanguages := TObjectList<TSLLanguages>.Create(True);
+  _ReadLanguagesFromIniFile(sllanguages);
 
-  if FileExists(ExtractFilePath(ParamStr(0)) + 'languagebase.slftp') then renameFile;
+  Debug(dpSpam, rsections, Format('<- Ready - Languages loaded: %d', [sllanguages.Count]));
 
-  if FileExists(ExtractFilePath(ParamStr(0)) + 'slftp.languagebase') then
-  begin
-    y := TStringList.Create;
-    try
-      y.LoadFromFile(ExtractFilePath(ParamStr(0)) + 'slftp.languagebase');
-      for i := 0 to y.Count - 1 do
-      begin
-        if ((y.Strings[i][1] = '[') and (y.Strings[i][length(y.Strings[i])] = ']')) then
-          Continue;
-
-        sllanguages.Add(TSLLanguages.Create('languages', SubString(y.Strings[i], '=', 1), SubString(y.Strings[i], '=', 2), i));
-      end;
-
-    finally
-      y.Free;
-    end;
-
-    slmp3languages := TStringList.Create;
-    slmp3languages.Delimiter := ' ';
-    slmp3languages.QuoteChar := '"';
-    slmp3languages.CaseSensitive := False;
-    slmp3languages.DelimitedText := UpperCase(config.ReadString('kb', 'mp3languages', ''));
-
-    Debug(dpSpam, rsections, '<- Ready - Languages loaded: ' + IntToStr(sllanguages.Count));
-  end;
-
+  slmp3languages := TStringList.Create;
+  SLLoadMP3LanguagesFromIniFile(slmp3languages);
+  Debug(dpSpam, rsections, Format('<- Ready - MP3 Languages loaded: %d', [slmp3languages.Count]));
 end;
 
-procedure SLLanguages_Uninit;
+procedure SLLanguagesUninit;
 begin
   Debug(dpSpam, rsections, 'Uninit1');
   sllanguages.Free;
   slmp3languages.Free;
-  sllanguagefile.Free;
   Debug(dpSpam, rsections, 'Uninit2');
+end;
+
+procedure SLLoadMP3LanguagesFromIniFile(var aStringList: TStringList);
+begin
+  aStringList.Clear;
+
+  aStringList.Delimiter := ' ';
+  aStringList.QuoteChar := '"';
+  aStringList.CaseSensitive := False;
+  aStringList.DelimitedText := UpperCase(config.ReadString('kb', 'mp3languages', ''));
 end;
 
 function SLLanguagesReload: String;
@@ -259,38 +138,81 @@ var
   i: integer;
   y: TStringList;
 begin
-  Debug(dpSpam, rsections, 'Reload Language Base...');
+  Debug(dpSpam, rsections, '-> Reload Language Base');
 
-  y := TStringList.Create;
+  _ReadLanguagesFromIniFile(sllanguages);
+  SLLoadMP3LanguagesFromIniFile(slmp3languages);
+
+  Result := Format('Reload done! %d languages and %d mp3 languages loaded.', [sllanguages.Count, slmp3languages.Count]);
+
+  Debug(dpSpam, rsections, '<- Reload Language Base');
+end;
+
+function FindLanguageOnDirectory(const aRlsname: String): String;
+var
+  i, j: integer;
+  lrx: TRegexpr;
+  sllang: TSLLanguages;
+  fRlsnameLowercase: String;
+begin
+  Result := 'English';
+  fRlsnameLowercase := LowerCase(aRlsname);
+
+  lrx := TRegexpr.Create;
   try
-    sllanguages.Clear;
-    y.LoadFromFile(ExtractFilePath(ParamStr(0)) + 'languagebase.slftp');
-    for i := 0 to y.Count - 1 do
+    lrx.ModifierI := True;
+
+    for sllang in sllanguages do
     begin
-      sllanguages.Add(TSLLanguages.Create('languages', SubString(y.Strings[i], '=', 1), SubString(y.Strings[i], '=', 2), i));
-      Result := Format('Reload done! %d languages loaded.', [sllanguages.Count]);
+      if sllang.NeedsRegexMatching then
+      begin
+        lrx.Expression := '[\.\-\_](' + sllang.Expression + ')[\.\-\_]';
+        if lrx.Exec(aRlsname) then
+        begin
+          Result := sllang.Language;
+          break;
+        end;
+      end
+      else
+      begin
+        j := Pos(sllang.Expression, fRlsnameLowercase);
+        if (j <> 0) then
+        begin
+          // make sure its [\.\-\_]language[\.\-\_]
+          if (aRlsname[j - 1] in ['.', '-', '_']) and (aRlsname[j + sllang.ExpressionLength] in ['.', '-', '_']) then
+          begin
+            Result := sllang.Language;
+            break;
+          end;
+        end;
+      end;
     end;
 
   finally
-    y.Free;
+    lrx.Free;
   end;
-
-  slmp3languages.Clear;
-  slmp3languages.Delimiter := ' ';
-  slmp3languages.QuoteChar := '"';
-  slmp3languages.CaseSensitive := False;
-  slmp3languages.DelimitedText := UpperCase(config.ReadString('kb', 'mp3languages', ''));
 end;
 
-procedure SLLanguagesRehash(out status: String);
+function FindMusicLanguageOnDirectory(const aRlsname: String): String;
 var
-  i: integer;
+  i, j: integer;
+  fRlsnameUppercase: String;
 begin
-  for i := 0 to sllanguages.Count - 1 do
-    TSLLanguages(sllanguages.Items[i]).Rehash;
+  Result := 'EN';
+  fRlsnameUppercase := UpperCase(aRlsname);
 
-  status := Format('Done! Languages count: %d', [sllanguages.Count]);
+  for i := 0 to slmp3languages.Count - 1 do
+  begin
+    j := Pos(slmp3languages[i], fRlsnameUppercase);
+    if (j <> 0) then
+    begin
+      if (aRlsname[j - 1] = '-') and (aRlsname[j + Length(slmp3languages[i])] = '-') then
+      begin
+        Result := slmp3languages[i];
+        break;
+      end;
+    end;
+  end;
 end;
 
 end.
-
