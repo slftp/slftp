da04fda6c9c494a52a50f3725e7cc1e3f3beb3ae
DEADC0DE <Dreamboxx@protonmail.com>
2019-07-22 19:24:09 +0200

    refactor(tags): Use enum + proper tests for TagComplete
    uses an enum value to identify return value of TagComplete now
    tests should only test against TagComplete

--- tags.pas
+++ tags.pas
@@ -2,83 +2,45 @@ unit tags;
 
 interface
 
+type
+  {
+  @abstract(Return types for TagComplete function)
+  @value(tctUNMATCHED percent dir matching and regex matching failed)
+  @value(tctINCOMPLETE it's an incomplete dir tag)
+  @value(tctCOMPLETE it's a complete dir tag)
+  }
+  TTagCompleteType = (tctUNMATCHED, tctINCOMPLETE, tctCOMPLETE);
+
+{ Just a helper function to init and validate user regex }
 procedure TagsInit;
+{ Just a helper function to uninit regex }
 procedure TagsUninit;
-function TagComplete(const filename: String): Integer;
-{ Searches for '% complete' in given @link(aFilename) and determines the percentage if found
+{ Tries to find complete status of given @link(aFilename)
   @param(aFilename complete dir/file)
-  @returns(0 if '% complete' not found, 1 if it's done (100%), otherwise -1.) }
-function CheckStandardPercentDir(const aFilename: String): Integer;
+  @returns(@link(tctCOMPLETE) if complete, @link(tctINCOMPLETE) if incomplete, otherwise @link(tctUNMATCHED).) }
+function TagComplete(const aFilename: String): TTagCompleteType;
 
 implementation
 
-uses Classes, SysUtils, mystrings, configunit, debugunit, FLRE, SyncObjs;
+uses
+  Classes, SysUtils, mystrings, configunit, debugunit, FLRE, SyncObjs;
 
 const
   section = 'tags';
 
 var
-  cs: TCriticalSection;
-  crc, cri: TFLRE;
+  cs: TCriticalSection; // lock to avoid concurrent access to regex objects
+  crc, cri: TFLRE; //< complete and incomplete regex object
 
-function TagComplete(const filename: String): Integer;
-begin
-  (*
-    Result:
-       0 if nothing matched (normal file/dir)
-       1 if file/dir is a complete tag
-      -1 if file/dir is an incomplete tag
-  *)
-
-  // check if the dir is a percent dir
-  Result := CheckStandardPercentDir(filename);
-  if Result <> 0 then
-    exit;
-
-  // regex matching
-  cs.Enter;
-  try
-    // is the file/dir a complete tag
-    try
-      if crc.Find(filename) <> 0 then
-      begin
-        Debug(dpSpam, section, 'TagComplete By FLRE %s', [filename]);
-        Result := 1;
-        exit;
-      end;
-    except
-      on e: Exception do
-      begin
-        Debug(dpError, section, Format('[EXCEPTION] TagComplete(crc): Exception : %s', [e.Message]));
-      end;
-    end;
-
-    // is the file/dir an incomplete tag
-    try
-      if cri.Find(filename) <> 0 then
-      begin
-        Debug(dpSpam, section, 'TagIncomplete By FLRE %s', [filename]);
-        Result := -1;
-        exit;
-      end;
-    except
-      on e: Exception do
-      begin
-        Debug(dpError, section, Format('[EXCEPTION] TagComplete(cri): Exception : %s', [e.Message]));
-      end;
-    end;
-  finally
-    cs.Leave;
-  end;
-
-end;
-
-function CheckStandardPercentDir(const aFilename: String): Integer;
+{ Fast search for '% complete' in given @link(aFilename) and determines the percentage if found
+  @param(aFilename complete dir/file)
+  @returns(tctUNMATCHED if '% complete' not found, tctCOMPLETE if it's done (100%), otherwise tctINCOMPLETE.) }
+function _CheckStandardPercentDir(const aFilename: String): TTagCompleteType;
 var
-  i, j: Integer;
+  i, j, max: Integer;
   fFoundNumber: Boolean;
 begin
-  Result := 0;
+  Result := tctUNMATCHED;
 
   i := Pos(UpperCase('% complete'), UpperCase(aFilename));
   if i > 4 then
@@ -101,14 +63,58 @@ begin
 
     if i = 100 then
     begin
-      Result := 1;
+      Result := tctCOMPLETE;
       exit;
     end
     else
     begin
-      Result := -1;
+      Result := tctINCOMPLETE;
       exit;
     end;
+  end;
+end;
+
+function TagComplete(const aFilename: String): TTagCompleteType;
+begin
+  // check if the dir is a percent dir
+  Result := _CheckStandardPercentDir(aFilename);
+  if Result <> tctUNMATCHED then
+    exit;
+
+  // regex matching
+  cs.Enter;
+  try
+    // is the file/dir a complete tag
+    try
+      if crc.Find(aFilename) <> 0 then
+      begin
+        Debug(dpSpam, section, 'TagComplete By FLRE %s', [aFilename]);
+        Result := tctCOMPLETE;
+        exit;
+      end;
+    except
+      on e: Exception do
+      begin
+        Debug(dpError, section, Format('[EXCEPTION] TagComplete(crc): Exception : %s', [e.Message]));
+      end;
+    end;
+
+    // is the file/dir an incomplete tag
+    try
+      if cri.Find(aFilename) <> 0 then
+      begin
+        Debug(dpSpam, section, 'TagIncomplete By FLRE %s', [aFilename]);
+        Result := tctINCOMPLETE;
+        exit;
+      end;
+    except
+      on e: Exception do
+      begin
+        Debug(dpError, section, Format('[EXCEPTION] TagComplete(cri): Exception : %s', [e.Message]));
+      end;
+    end;
+  finally
+    cs.Leave;
   end;
 end;
 
@@ -120,8 +126,8 @@ var
 begin
   Debug(dpSpam, section, 'Init %s begins', [section]);
 
-  complete_regex_default := '([^\w]*100%[^\w]*)|([^\w]*-\sCOMPLETE\s\)[^\w]*)|([^\w]*-\sCOMPLETE\s-[^\w]*)|([^\w].*DONE\s\-\>\s\d+F[^\w]*)|((\dM\s*\dF.*?|\d+[\w]*[^\w]*\d+[\w]*[^\w]*)(DONE|COMPLETE$|FINISH)|(\d+F[^\w].*DONE)|COMPLETE\-\d+M\_\d+F)';
-  incomplete_regex_default := '(\d{1,2}\s*%\s*Complete|incomplete)';
+  complete_regex_default := '([^\w]*100%[^\w]*)|([^\w]*-\sCOMPLETE\s\)[^\w]*)|([^\w]*-\sCOMPLETE\s-[^\w]*)|([^\w].*DONE\s\-\>\s\d+F[^\w]*)|((\dM\s*\dF.*?|\d+[\w]*[^\w]*\d+[\w]*[^\w]*)(DONE|COMPLETE$|FINISH)|(\d+F[^\w].*DONE)|COMPLETE\-\d+M\_\d+F||Completed!.*?\d+)';
+  incomplete_regex_default := '(\d{1,2}\s*%\s*Complete|incomplete|\d{1,2}%|\-\s*\d{1,2}DONE)';
 
   dummy_string := '[xy] - ( 19M 4F - COMPLETE ) - [xy]';
 
@@ -138,6 +144,7 @@ begin
     begin
       if Assigned(crc) then
         crc.Free;
+
       Debug(dpError, section, Format('TagComplete: slftp.ini complete_regex is invalid. Falling back to default. (Exception :%s)', [e.Message]));
       crc := TFLRE.Create(complete_regex_default, [rfIGNORECASE]);
     end;
@@ -154,6 +161,7 @@ begin
     begin
       if Assigned(cri) then
         cri.Free;
+
       Debug(dpError, section, Format('TagComplete: slftp.ini incomplete_regex is invalid. Falling back to default. (Exception :%s)', [e.Message]));
       cri := TFLRE.Create(incomplete_regex_default, [rfIGNORECASE]);
     end;
