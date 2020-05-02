f5466323023793ea003a5205e86fbada2798bea8
Dreamboxx <Dreamboxx@protonmail.com>
2020-05-02 23:01:34 +0200

    refactor(ParseDirlist): Separate functions, renaming, comments
    - extracted some functionality into own functions
    - renamed some variables
    - global_skip variable in globals.pas now
    - comments and example inputs + tests

--- globals.pas
+++ globals.pas
@@ -12,6 +12,9 @@ const
   SiteColorUnknown = 'c14'; //< Grey color for @link(sitesunit.TSiteStatus.sstUnknown)
   DATABASEFOLDERNAME = 'databases'; //< foldername for all internal database files
 
+var
+  GlobalSkiplistRegex: String; //< global_skip regex from slftp.ini
+
 implementation
 
 end.
