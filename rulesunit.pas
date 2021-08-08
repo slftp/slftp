32d9b841ea1aa866b35d45478c270237d8af1b84
DEADC0DE <Dreamboxx@protonmail.com>
2021-08-08 10:59:48 +0000

    fix(rtpl): persist all site rules when split_site_data is enabled
    Rules for all sites (indicated with identifier '*') were not being persisted if split_site_data was enabled.
    Those are now saved into <admin_sitename>.rtpl

--- rulesunit.pas
+++ rulesunit.pas
@@ -1770,57 +1770,60 @@ end;
 
 procedure RulesSave;
 var
-  i: integer;
-  f: TEncStringlist;
-  a_i: integer;
-  a_j: integer;
-  a_sitename: String;
-  a_rules_path: String;
-  a_sites_done: TStringList;
-  a_siterules: TStringList;
-  a_r: TRule;
+  i, j: integer;
+  fEncStringlist: TEncStringlist;
+  fSitename: String;
+  fRulesPath: String;
+  fSitesDone: TStringList;
+  fSiteRules: TStringList;
+  fRule: TRule;
 begin
   if (config.ReadBool('sites', 'split_site_data', False)) then
   begin
-    a_sites_done := TStringList.Create;
+    fSitesDone := TStringList.Create;
     try
-      a_rules_path := ExtractFilePath(ParamStr(0)) + 'rtpl' + PathDelim;
-      for a_i := 0 to rules.Count - 1 do
+      fRulesPath := ExtractFilePath(ParamStr(0)) + 'rtpl' + PathDelim;
+      for i := 0 to rules.Count - 1 do
       begin
-        a_r := TRule(rules[a_i]);
-        a_sitename := a_r.sitename;
-        if (a_sites_done.IndexOf(a_sitename) <> -1) or (a_sitename = '*') then
+        fRule := TRule(rules[i]);
+        fSitename := fRule.sitename;
+
+        if (fSitesDone.IndexOf(fSitename) <> -1) then
           continue;
 
-        a_siterules := TStringList.Create;
+        fSiteRules := TStringList.Create;
         try
-          for a_j := a_i to rules.Count - 1 do
+          for j := i to rules.Count - 1 do
           begin
-            a_r := TRule(rules[a_j]);
-            if a_r.sitename <> a_sitename then
+            fRule := TRule(rules[j]);
+            if fRule.sitename <> fSitename then
               continue;
-            a_siterules.Add(a_r.AsText(True));
+            fSiteRules.Add(fRule.AsText(True));
           end;
 
-          a_siterules.SaveToFile(a_rules_path + a_sitename + '.rtpl');
-          a_sites_done.Add(a_sitename);
+          if fSitename = '*' then
+            fSiteRules.SaveToFile(fRulesPath + getAdminSiteName + '.rtpl')
+          else
+            fSiteRules.SaveToFile(fRulesPath + fSitename + '.rtpl');
+
+          fSitesDone.Add(fSitename);
         finally
-          a_siterules.Free;
+          fSiteRules.Free;
         end;
       end;
     finally
-      a_sites_done.Free;
+      fSitesDone.Free;
     end;
   end
   else
   begin
-    f := TEncStringlist.Create(passphrase);
+    fEncStringlist := TEncStringlist.Create(passphrase);
     try
       for i := 0 to rules.Count - 1 do
-        f.Add(TRule(rules[i]).AsText(True));
-      f.SaveToFile(ExtractFilePath(ParamStr(0)) + 'slftp.rules');
+        fEncStringlist.Add(TRule(rules[i]).AsText(True));
+      fEncStringlist.SaveToFile(ExtractFilePath(ParamStr(0)) + 'slftp.rules');
     finally
-      f.Free;
+      fEncStringlist.Free;
     end;
   end;
 end;
