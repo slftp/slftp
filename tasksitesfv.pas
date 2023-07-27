ea8c010bcbba421591d1a522356628b28c2ac458
PES <pesdev@protonmail.com>
2023-07-27 22:29:16 +0200

    fix(sfv): show sitename when logging SFV download

--- tasksitesfv.pas
+++ tasksitesfv.pas
@@ -148,7 +148,7 @@ begin
       // SFV file was downloaded. Parse
       self.mainpazo.PazoSFV.SetSFVList(FDir, ParseSFV(fStream.DataString));
 
-      irc_SendUPDATE(Format('<c3>[SFV]</c> %s %s now has SFV infos', [mainpazo.rls.section, fRelativePath]));
+      irc_SendUPDATE(Format('<c3>[SFV]</c> %s %s now has SFV infos (%s)', [mainpazo.rls.section, fRelativePath, self.site1]));
     except
       on e: Exception do
       begin
