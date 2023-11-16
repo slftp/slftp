1f298badc890fe83e5e2bdf5ae02a281d2e64153
PES <pesdev@protonmail.com>
2023-11-16 22:40:59 +0100

    fix(removepazo): set slots to nil to make the queue remove the task

--- queueunit.pas
+++ queueunit.pas
@@ -1205,7 +1205,7 @@ begin
               begin
                 Debug(dpMessage, section, Format('RemovePazo: Force removal of assigned task: %s', [t.Name]));
                 t.readyerror := True;
-                if (t.slot1 <> nil) and (TSiteSlot(t.slot1).todotask = t) then
+                if TSiteSlot(t.slot1).todotask = t then
                 begin
                   with TSiteSlot(t.slot1) do
                   begin
@@ -1213,6 +1213,9 @@ begin
                     site.RebuildSlot(SlotNumber);
                   end;
                 end;
+
+                t.slot1 := nil;
+                t.slot2 := nil;
               end;
             end;
           end;
