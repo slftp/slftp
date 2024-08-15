b537b617c590f0435c44230edb01f461e0dd4fed
DEADC0DE <Dreamboxx@protonmail.com>
2024-08-15 23:08:46 +0200

    refactor(globals): remove dupe file for global constants

--- globals.pas
+++ globals.pas
@@ -11,6 +11,8 @@ const
   SiteColorPermdown = 'c5'; //< Brown color if site is set to permdown
   SiteColorUnknown = 'c14'; //< Grey color for @link(sitesunit.TSiteStatus.sstUnknown)
   DATABASEFOLDERNAME = 'databases'; //< foldername for all internal database files
+  CONST_RAR_FILES = '_RAR_'; //< pseudo file extension to cover all RAR files
+  CONST_NFO_FAILED_THRESHOLD = 4; //< number of consecutive failures when downloading NFO (or SFV) files to set ufnAutoDisabled on a site
 
 implementation
 
