2189717aedfaeeb697a4008decf3e999d9bdb5ce
PES <pesdev@protonmail.com>
2025-04-24 23:26:47 +0200

    fix(freeslots): need a lock for freeslots and RecalcFreeSlots

--- sitesunit.pas
+++ sitesunit.pas
@@ -228,6 +228,7 @@ type
     fKillConnectionOnStalledTransferSeconds: integer;
     fSpeedFromCS: TSlCriticalSection2;
     fSpeedFromCache: TStringList;
+    fFreeSlotsCS: TSlCriticalSection2;
     const FDefaultSslMethod: TSSLMEthods = sslAuthTls;
     function GetSkipPreStatus: boolean;
     procedure SetSkipPreStatus(Value: boolean);
@@ -288,8 +289,6 @@ type
     function FetchAutoRules: TRulesTask;
     function FetchAutoDirlist: TAutoDirlistTask;
     function FetchAutoNuke: TAutoNukeTask;
-    procedure SetNumDn(const Value: integer);
-    procedure SetNumUp(const Value: integer);
     procedure SetFreeSlots(const Value: integer);
 
     { function for @link(ProxyName) property to read proxyname from inifile (default value: !!NOIN!!) }
@@ -542,8 +541,8 @@ type
     property sectionprecmd[Name: String]: String read GetSectionPreCmd write SetSectionPrecmd;
     property siteaffils: String read GetAffils write SetAffils;
     property sectionpretime[const Name: String]: integer read GetSectionPreTime write SetSectionPreTime;
-    property num_dn: integer read fNumDn write SetNumDn;
-    property num_up: integer read fNumUp write SetNumUp;
+    property num_dn: integer read fNumDn write fNumDn;
+    property num_up: integer read fNumUp write fNumUp;
     property delayleech[const aSection: String]: integer read GetDelayLeech; //< returns random value between min and max seconds for delaying leech
     property delayupload[const aSection: String]: integer read GetDelayUpload; //< returns random value between min and max seconds for delaying upload
     property freeslots: integer read fFreeslots write SetFreeSlots;
@@ -2998,13 +2997,13 @@ begin
     fDownloadingFrom := Value;
     if fDownloadingFrom then
     begin
-      {$IFDEF FPC}InterlockedIncrement{$ELSE}AtomicIncrement{$ENDIF}(site.num_dn);
+      {$IFDEF FPC}InterlockedIncrement{$ELSE}AtomicIncrement{$ENDIF}(site.fNumDn);
       if GetDebugVerbosity = dpSpam then
         Debug(dpSpam, section, 'Site %s: Download slots in use: %d!', [site.Name,site.num_dn ]);
     end
     else
     begin
-      {$IFDEF FPC}InterlockedDecrement{$ELSE}AtomicDecrement{$ENDIF}(site.num_dn);
+      {$IFDEF FPC}InterlockedDecrement{$ELSE}AtomicDecrement{$ENDIF}(site.fNumDn);
       if GetDebugVerbosity = dpSpam then
         Debug(dpSpam, section, 'Site %s: Download slots in use: %d!', [site.Name,site.num_dn ]);
     end;
@@ -3018,13 +3017,13 @@ begin
     fUploadingTo := Value;
     if fUploadingTo then
       begin
-        {$IFDEF FPC}InterlockedIncrement{$ELSE}AtomicIncrement{$ENDIF}(site.num_up);
+        {$IFDEF FPC}InterlockedIncrement{$ELSE}AtomicIncrement{$ENDIF}(site.fNumUp);
         if GetDebugVerbosity = dpSpam then
           Debug(dpSpam, section, 'Site %s: Upload slots in use: %d!', [site.Name,site.num_up ]);
       end
     else
       begin
-        {$IFDEF FPC}InterlockedDecrement{$ELSE}AtomicDecrement{$ENDIF}(site.num_up);
+        {$IFDEF FPC}InterlockedDecrement{$ELSE}AtomicDecrement{$ENDIF}(site.fNumUp);
         if GetDebugVerbosity = dpSpam then
           Debug(dpSpam, section, 'Site %s: Upload slots in use: %d!', [site.Name,site.num_up ]);
       end;
@@ -3035,19 +3034,23 @@ procedure TSiteSlot.SetTodotask(Value: TTask);
 begin
   if fTodotask <> Value then
   begin
-    fTodotask := Value;
-    if fTodoTask <> nil then
+    site.fFreeSlotsCS.Enter('SetTodotask');
+    try
+      fTodotask := Value;
+      if fTodoTask <> nil then
       begin
-        {$IFDEF FPC}InterlockedDecrement{$ELSE}AtomicDecrement{$ENDIF}(site.freeslots);
-        if GetDebugVerbosity = dpSpam then
-          Debug(dpSpam, section, 'Site %s: Free slots: %d!', [site.Name,site.freeslots ]);
-        end
-    else
+        site.freeslots := site.freeslots - 1;
+      end
+      else
       begin
-        {$IFDEF FPC}InterlockedIncrement{$ELSE}AtomicIncrement{$ENDIF}(site.freeslots);
-        if GetDebugVerbosity = dpSpam then
-          Debug(dpSpam, section, 'Site %s: Free slots: %d!', [site.Name,site.freeslots ]);
+        site.freeslots := site.freeslots + 1;
       end;
+    finally
+      site.fFreeSlotsCS.Leave;
+    end;
+
+    if GetDebugVerbosity = dpSpam then
+      Debug(dpSpam, section, 'Site %s: Free slots: %d!', [site.Name,site.freeslots ]);
   end;
 end;
 
@@ -3066,6 +3069,7 @@ begin
   fQueue := TQueueThread.Create(Name);
   self.fSpeedFromCS := TSlCriticalSection2.Create('SpeedFromCS_' + Name);
   self.fSpeedFromCache := nil;
+  self.fFreeSlotsCS := TSlCriticalSection2.Create('FreeSlotsCS_' + Name);
 
   if (Name = getAdminSiteName) then
   begin
@@ -3245,6 +3249,7 @@ begin
   fSlotsAssignmentLock.Free;
   fSpeedFromCS.Free;
   FreeAndNil(fSpeedFromCache);
+  fFreeSlotsCS.Free;
   Debug(dpSpam, section, 'Site %s destroy end', [Name]);
   inherited;
 end;
@@ -4168,18 +4173,6 @@ begin
     Result := StrToIntDef(mdtmre.Match[6], 0);
 end;
 
-procedure TSite.SetNumDn(const Value: integer);
-begin
-  if Value >= 0 then
-    fNumDn := Value;
-end;
-
-procedure TSite.SetNumUp(const Value: integer);
-begin
-  if Value >= 0 then
-    fNumUp := Value;
-end;
-
 procedure TSite.SetFreeSlots(const Value: integer);
 begin
   if Value >= 0 then
@@ -4192,15 +4185,20 @@ var
   ss: TSiteSlot;
   fs: integer;
 begin
-  fs := 0;
-  for i := 0 to slots.Count - 1 do
-  begin
-    ss := TSiteSlot(slots[i]);
-    if ss.todotask = nil then
-      Inc(fs);
-  end;
+  fFreeSlotsCS.Enter('RecalcFreeslots');
+  try
+    fs := 0;
+    for i := 0 to slots.Count - 1 do
+    begin
+      ss := TSiteSlot(slots[i]);
+      if ss.todotask = nil then
+        Inc(fs);
+    end;
 
-  ffreeslots := fs;
+    ffreeslots := fs;
+  finally
+    fFreeSlotsCS.Leave;
+  end;
 end;
 
 procedure TSite.FullLogin;
