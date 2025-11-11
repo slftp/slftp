47ac289e6af2704abeaf14426825edf04433ab0d
Dreamboxx <Dreamboxx@protonmail.com>
2025-11-11 10:28:10 +0100

    fix(pazo): fix merge issue

--- pazo.pas
+++ pazo.pas
@@ -792,7 +792,7 @@ begin
     FUniqueFileListOfRelease_cs.Leave;
   end;
 
- if fWasAdded and aIsSFV and (self.rls <> nil) and (FPazoSFV <> nil) and self.rls.IsSFVRelease and not FPazoSFV.HasSFV(aDir) then
+ if fWasAdded and de.IsSFV and (self.rls <> nil) and (FPazoSFV <> nil) and self.rls.IsSFVRelease and not FPazoSFV.HasSFV(aDir) then
 
   begin
     if FPazoSFV.RegisterSFV(aDir) then
