5ebb7df7ff689ade7de121a55ee60f7945d16fca
Moe84 <moe201484@protonmail.com>
2025-12-25 21:19:50 +0100

    fix(stats): add new file for tests

---
+++ tests/statsunitTests.pas
@@ -0,0 +1,109 @@
+unit statsunitTests;
+
+interface
+
+uses
+  {$IFDEF FPC}
+    TestFramework,
+  {$ELSE}
+    DUnitX.TestFramework, DUnitX.DUnitCompatibility,
+  {$ENDIF}
+  SysUtils, statsunit, mormot.rest.sqlite3, mormot.orm.core, Generics.Collections;
+
+type
+  TTestStatsUnit = class(TTestCase)
+  private
+    procedure WaitForQueue;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestInitAndActive;
+    procedure TestProcessRace;
+    procedure TestRemoveStats;
+  end;
+
+implementation
+
+uses
+  configunit, sitesunit, mormot.core.base, slcriticalsection2;
+
+{ TTestStatsUnit }
+
+procedure TTestStatsUnit.SetUp;
+begin
+  inherited SetUp;
+  if not IsStatsDatabaseActive then
+  begin
+    // Force initialization with a test database name
+    glDeleteAfterDays := 0;
+    glStatsModel := TSQLModel.Create([TSQLStatsRecord, TSQLSitesRecord, TSQLSectionRecord, TSQLFileInfoRecord]);
+    glStatsDb := TSQLRestClientDB.Create(glStatsModel, nil, 'test_stats.db', TSQLRestServerDB);
+    TSQLRestServerDB(glStatsDb.Server).CreateMissingTables;
+    
+    glStatRaceQueue := TQueue<TStatRaceRecord>.Create;
+    glStatRaceLock := TSlCriticalSection2.Create('glStatRaceLock');
+    TWriteStatsToDBThread.Create;
+  end;
+end;
+
+procedure TTestStatsUnit.TearDown;
+begin
+  inherited TearDown;
+end;
+
+procedure TTestStatsUnit.WaitForQueue;
+var
+  i: integer;
+begin
+  // Give the background thread some time to process
+  for i := 1 to 10 do
+  begin
+    Sleep(200);
+  end;
+end;
+
+procedure TTestStatsUnit.TestInitAndActive;
+begin
+  CheckTrue(IsStatsDatabaseActive, 'Stats database should be active after init');
+  CheckNotNull(glStatsDb, 'glStatsDb should not be nil');
+  CheckNotNull(glStatsModel, 'glStatsModel should not be nil');
+end;
+
+procedure TTestStatsUnit.TestProcessRace;
+var
+  fCountBefore, fCountAfter: integer;
+begin
+  fCountBefore := glStatsDb.TableRowCount(TSQLStatsRecord);
+  
+  statsProcessRace('SRC_SITE', 'DST_SITE', 'SECTION', 'RELEASE.NAME', 'file.rar', 500000);
+  
+  WaitForQueue;
+  
+  fCountAfter := glStatsDb.TableRowCount(TSQLStatsRecord);
+  CheckTrue(fCountAfter > fCountBefore, 'Stats record count should have increased');
+end;
+
+procedure TTestStatsUnit.TestRemoveStats;
+var
+  fSiteCountBefore, fSiteCountAfter: integer;
+begin
+  // Add a site entry first by processing a race
+  statsProcessRace('DELETE_ME_SRC', 'DELETE_ME_DST', 'SEC', 'RLS', 'file.rar', 600000);
+  WaitForQueue;
+  
+  fSiteCountBefore := glStatsDb.TableRowCount(TSQLSitesRecord);
+  
+  CheckTrue(RemoveStats('DELETE_ME_SRC'), 'RemoveStats should return true');
+  
+  fSiteCountAfter := glStatsDb.TableRowCount(TSQLSitesRecord);
+  CheckTrue(fSiteCountAfter < fSiteCountBefore, 'Site record count should have decreased');
+end;
+
+initialization
+  {$IFDEF FPC}
+    RegisterTest('statsunit', TTestStatsUnit.Suite);
+  {$ELSE}
+    TDUnitX.RegisterTestFixture(TTestStatsUnit);
+  {$ENDIF}
+end.
