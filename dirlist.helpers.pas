5c9f744963c1ed198f725f60a283ddcc0da15f95
Moe84 <pesdev@protonmail.com>
2026-01-05 12:12:10 +0100

    fix(dirlist): add stricter UNIX permission format validation
    Enhanced the filter logic to require both conditions:
    1. Line must start with 'd' (directory) or '-' (file)
    2. Position 11 must be a space (UNIX permission format)
    
    This provides double protection against FTP status messages:
    - "213- status of -l:" fails check 1 (starts with '2')
    - "213- status of -l:" fails check 2 (position 11 is '-', not space)
    - "total 12345" fails check 1 (starts with 't')
    
    Changed from: if (not 'd' and not '-') then skip
    To: if ((not 'd' and not '-') OR (pos 11 != space)) then skip

--- dirlist.helpers.pas
+++ dirlist.helpers.pas
@@ -259,9 +259,9 @@ begin
       if fLineToParse = '' then continue;
       if (Length(fLineToParse) > 11) then
       begin
-        // Only process lines that start with 'd' (directory) or '-' (file)
-        // This filters out FTP status messages like "213- status of -l:" or "total 12345"
-        if (fLineToParse[1] <> 'd') and (fLineToParse[1] <> '-') then
+        // Only process lines that match UNIX permission format: 'd' or '-' at position 1, space at position 11
+        // This filters out FTP status messages like "213- status of -l:", "total 12345", "213 End of Status"
+        if ((fLineToParse[1] <> 'd') and (fLineToParse[1] <> '-')) or (fLineToParse[11] <> ' ') then
           continue;
         ParseStatResponseLine(string(fLineToParse), fDirMask, fUsername, fGroupname, fFilesize, fDatum, fFilename);
         fParsedDirlistEntry := TParsedDirlistEntry.Create;
