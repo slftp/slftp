568ede6f2009bfea44aaf0350c5468ad71990b9b
Dreamboxx <Dreamboxx@protonmail.com>
2026-01-25 19:50:46 +0100

    fix(imdb): fix PostResults method signature for persistent database

--- dbaddimdb.pas
+++ dbaddimdb.pas
@@ -140,7 +140,7 @@ TIMDbBomDataRecord = class(TOrmNoCase)
     UpdatedTime: TDateTime;
     constructor Create(const aIMDbId:String);
     destructor Destroy; override;
-    procedure PostResults(const aRls : String = '');
+    procedure PostResults(const aRls: String);
     procedure SetIMDBRelease(ir: TIMDBRelease);
 end;
 
@@ -852,47 +852,6 @@ begin
   imdb_countries.Free;
   imdb_genres.Free;
   inherited;
-end;
-
-procedure TDbImdbData.PostResults(rls : String = '');
-var status:String;
-begin
-
-  if imdb_stvm then status := 'STV'
-  else if imdb_festival then status := 'Festival'
-  else if imdb_ldt then status := 'Limited'
-  else if imdb_wide then status := 'Wide'
-  else status :='Cine';
-
-  irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c0><b>for : %s</b></c> .......: https://www.imdb.com/title/%s/',[rls, imdb_id]));
-  irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c0><b>Original Title - Year</b></c> ...: %s (%d)',[imdb_origtitle, imdb_year]));
-  irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <b><c9>Country - Languages</b></c> ..: %s - %s',[imdb_countries.DelimitedText,imdb_languages.DelimitedText]));
-  irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <b><c5>Genres</b></c> .........: %s', [imdb_genres.DelimitedText]));
-
-  if imdb_screens >= 0 then
-    irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c7><b>Rating</b>/<b>Type</b></c> ....: <b>%d</b> of 100 (%d) @ %d Screens (%s) | Type: %s',[imdb_rating,imdb_votes,imdb_screens,status,imdb_type]))
-  else
-    irc_Addstats(Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c7><b>Rating</b>/<b>Type</b></c> ....: <b>%d</b> of 100 (%d) (%s) | Type: %s',[imdb_rating,imdb_votes,status,imdb_type]));
-end;
-
-procedure TDbImdbData.PostResults(const netname, channel: String; rls : String = '');
-var status:String;
-begin
-  if imdb_stvm then status := 'STV'
-  else if imdb_festival then status := 'Festival'
-  else if imdb_ldt then status := 'Limited'
-  else if imdb_wide then status := 'Wide'
-  else status :='Cine';
-
-  irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c0><b>for : %s</b></c> .......: https://www.imdb.com/title/%s/',[rls, imdb_id]));
-  irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c0><b>Original Title - Year</b></c> ...: %s (%d)',[imdb_origtitle, imdb_year]));
-  irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <b><c9>Country - Languages</b></c> ..: %s - %s',[imdb_countries.DelimitedText,imdb_languages.DelimitedText]));
-  irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <b><c5>Genres</b></c> .........: %s', [imdb_genres.DelimitedText]));
-
-  if imdb_screens >= 0 then
-    irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c7><b>Rating</b>/<b>Type</b></c> ....: <b>%d</b> of 100 (%d) @ %d Screens (%s) | Type: %s',[imdb_rating,imdb_votes,imdb_screens,status,imdb_type]))
-  else
-    irc_AddText(netname, channel, Format('(<c9>i</c>).....<c2><b>IMDB</b></c>........ <c7><b>Rating</b>/<b>Type</b></c> ....: <b>%d</b> of 100 (%d) (%s) | Type: %s',[imdb_rating,imdb_votes,status,imdb_type]));
 end;
 
 procedure TDbImdbData.PostResults(const aRls: String);
