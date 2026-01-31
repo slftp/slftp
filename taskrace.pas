0f058c35d3dc7a7b28f71438ebce100bf7be450f
Dreamboxx <Dreamboxx@protonmail.com>
2026-01-31 21:48:57 +0100

    fix(taskrace): prefer quit relogin

--- taskrace.pas
+++ taskrace.pas
@@ -324,7 +324,7 @@ begin
     case s.lastResponseCode of
       421:
         begin
-          s.DestroySocketAndRelogin('TPazoDirlistTask');
+          s.QuitAndRelogin('TPazoDirlistTask');
           goto TryAgain;
         end;
 
@@ -2076,7 +2076,7 @@ begin
             if spamcfg.readbool(c_section, 'cant_open_data_connection', True) then
               irc_Adderror(Format('<c4>[Can''t open data connection]</c> %s : %d %s', [tname, lastResponseCode, LeftStr(lastResponse, 90)]));
 
-            sdst.DestroySocketAndRelogin('TPazoRaceTask');
+            sdst.QuitAndRelogin('TPazoRaceTask');
             mainpazo.errorreason := 'Can''t open data connection';
             readyerror := True;
             Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2089,7 +2089,7 @@ begin
             if spamcfg.readbool(c_section, 'cant_open_data_connection', True) then
               irc_Adderror(sdst.todotask, '<c4>[ERROR Cant build]</c> TPazoRaceTask %s', [tname]);
 
-            sdst.DestroySocketAndRelogin('TPazoRaceTask');
+            sdst.QuitAndRelogin('TPazoRaceTask');
             mainpazo.errorreason := 'Timeout or building data connection problem';
             readyerror := True;
             Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2216,7 +2216,7 @@ begin
             begin
               if sdst.site <> nil then
                 sdst.site.RegisterMaxSimUpHit(sdst.Name);
-              sdst.DestroySocketAndRelogin('Maximum of simultaneous uploads reached');
+              sdst.QuitAndRelogin('Maximum of simultaneous uploads reached');
             end;
 
             mainpazo.errorreason := 'Maximum of simultaneous uploads reached';
@@ -2318,7 +2318,7 @@ begin
             begin
               if sdst.site <> nil then
                 sdst.site.RegisterMaxSimUpHit(sdst.Name);
-              sdst.DestroySocketAndRelogin('Maximum of simultaneous uploads reached');
+              sdst.QuitAndRelogin('Maximum of simultaneous uploads reached');
             end;
 
             mainpazo.errorreason := 'Maximum of simultaneous uploads reached';
@@ -2367,7 +2367,7 @@ begin
       irc_Adderror(sdst.todotask, '<c4>[ERROR FXP]</c> TPazoRaceTask %s: %s %d %s', [sdst.Name, tname, lastResponseCode, LeftStr(lastResponse, 90)]);
 
       mainpazo.errorreason := Format('Unhandled error %s after STOR (%s) : %d %s', [sdst.site.Name, tname, lastResponseCode, LeftStr(lastResponse, 90)]);
-      sdst.DestroySocketAndRelogin('TPazoRaceTask');
+      sdst.QuitAndRelogin('TPazoRaceTask');
       readyerror := True;
       Debug(dpMessage, c_section, '<- ' + tname);
       exit;
@@ -2425,7 +2425,7 @@ begin
             if spamcfg.readbool(c_section, 'cant_open_data_connection', True) then
               irc_Adderror(ssrc.todotask, '<c4>[ERROR Cant open]</c> TPazoRaceTask %s', [tname]);
 
-              sdst.DestroySocketAndRelogin('TPazoRaceTask');
+              sdst.QuitAndRelogin('TPazoRaceTask');
               mainpazo.errorreason := 'Timeout or opening data connection problem';
               readyerror := True;
               Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2441,7 +2441,7 @@ begin
 
 
               // maybe remove the source from race because fxp isn't allowed?
-              sdst.DestroySocketAndRelogin('TPazoRaceTask');
+              sdst.QuitAndRelogin('TPazoRaceTask');
               mainpazo.errorreason := 'Opening data connection problem';
               readyerror := True;
               Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2501,7 +2501,7 @@ begin
             // need to have first coded TSite.LastCredits to get it work somehow
             ssrc.site.SetKredits;
 
-            sdst.DestroySocketAndRelogin('TPazoRaceTask');
+            sdst.QuitAndRelogin('TPazoRaceTask');
             mainpazo.errorreason := 'Out of credits';
             readyerror := True;
             Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2524,7 +2524,7 @@ begin
             if spamcfg.readbool(c_section, 'permission_denied', True) then
               irc_Adderror(ssrc.todotask, '<c4>[ERROR] Permission denied</c> %s', [tname]);
 
-            sdst.DestroySocketAndRelogin('TPazoRaceTask');
+            sdst.QuitAndRelogin('TPazoRaceTask');
             mainpazo.errorreason := 'Permission denied';
             readyerror := True;
             Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2539,7 +2539,7 @@ begin
               irc_Adderror(ssrc.todotask, '<c4>[ERROR] Permission denied</c> %s', [tname]);
 
             //TODO: Disable downloading for this site for some time until you uploaded more stuff to download again
-            sdst.DestroySocketAndRelogin('TPazoRaceTask');
+            sdst.QuitAndRelogin('TPazoRaceTask');
             mainpazo.errorreason := 'Permission denied - limit of bandwidth usage detected';
             readyerror := True;
             Debug(dpSpam, c_section, '<- ' + mainpazo.errorreason + ' ' + tname);
@@ -2573,7 +2573,7 @@ begin
             begin
               if ssrc.site <> nil then
                 ssrc.site.RegisterMaxSimDownHit(ssrc.Name);
-              ssrc.DestroySocketAndRelogin('Maximum of simultaneous downloads reached');
+              ssrc.QuitAndRelogin('Maximum of simultaneous downloads reached');
             end;
 
             if sdst <> nil then
