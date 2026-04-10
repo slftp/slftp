slftp/slftp

commit ffb6563d1ea3b39f67181c61e4d648a6a521cd86

feat(udp): port UDP/cbftp routing from udp_cbftp_support branch
Port full UDP/cbftp integration from remotes/origin/udp_cbftp_support:
- pazo.pas: add FUDPEnabled/FUDPIp/FUDPPort/FUDPPassword/FUDPConfigLoaded
  fields, LoadUDPConfig procedure, mORMot2 UDP socket send in RoutesText,
  early exit in Tuzelj when UDP active, skip SFV/IRC stats when UDP enabled,
  allow offline/permdown sites in AddSites when UDP enabled
- kb.pas: skip dirlist task creation and IRC noise when UDP enabled,
  allow down-site events to be processed when UDP enabled
- rulesunit.pas: allow permdown/offline sites in FireRules when UDP enabled
- dbaddpre.pas: force kb_Add when UDP enabled even without add_to_kb flag
- taskgenredirlist/taskgenrenfo/tasksitenfo.pas: skip gracefully when UDP active
- config/slftp.ini: add [UDPConfig] section with default values

kb.pas
@@ -209,6 +209,19 @@ var
   l: TLoginTask;
   fPretimeLookupTask: TPazoPretimeLookupTask;
 
+  function IsUDPEnabled: Boolean;
+  var
+    rawEnable: String;
+    udpIp: String;
+    udpPort: Integer;
+  begin
+    rawEnable := Trim(config.ReadString('UDPConfig', 'EnableUDP', 'False'));
+    udpIp := Trim(config.ReadString('UDPConfig', 'IP', ''));
+    udpPort := config.ReadInteger('UDPConfig', 'Port', 0);
+    Result := (SameText(rawEnable, 'True') or SameText(rawEnable, '1')) and
+      (udpIp <> '') and (udpPort >= 1) and (udpPort <= 65535);
+  end;
+
   { Removes the oldest knowledge base entries }
   procedure KbListsCleanUp;
   begin
@@ -435,8 +448,9 @@ begin
       if (event = kbeCOMPLETE) then
       begin
         // complet an old rls not in kb
-        irc_Addstats(Format('<c7>[COMPLETE]</c> %s %s @ %s (not in kb)',
-          [section, rls, '<b>' + sitename + '</b>']));
+        if not IsUDPEnabled then
+          irc_Addstats(Format('<c7>[COMPLETE]</c> %s %s @ %s (not in kb)',
+            [section, rls, '<b>' + sitename + '</b>']));
         exit;
       end;
 
@@ -671,7 +685,7 @@ begin
     end;
 
     s := FindSiteByName(netname, psource.Name);
-    if ((s <> nil) and (not (s.WorkingStatus in [sstUnknown, sstUp]))) then
+    if ((s <> nil) and (not p.IsUDPEnabled) and (not (s.WorkingStatus in [sstUnknown, sstUp]))) then
       exit;
 
     psource.ircevent := True;
@@ -810,66 +824,65 @@ begin
 
   // status changed
   ss := p.RoutesText;
-  if ss <> '' then
-  begin
-    irc_SendROUTEINFOS(ss);
-  end;
 
   if (psource <> nil) and (psource.Status = rssNotAllowed) then
   begin
     psource.Status := rssNotAllowedButItsThere;
   end;
 
-  // now add dirlist
+  // now add dirlist (skip when UDP is enabled)
   try
     if (event in [kbeNEWDIR, kbePRE, kbeSPREAD, kbeADDPRE, kbeUPDATE]) then
     begin
-      for i := p.PazoSitesList.Count - 1 downto 0 do
+      if not p.IsUDPEnabled then
       begin
-        try
-          if i < 0 then
+        for i := p.PazoSitesList.Count - 1 downto 0 do
+        begin
+          try
+            if i < 0 then
+              Break;
+          except
             Break;
-        except
-          Break;
-        end;
-        try
-          ps := TPazoSite(p.PazoSitesList[i]);
-
-          // dirlist not available
-          if ps.dirlist = nil then
-          begin
-            Debug(dpError, section, 'ERROR: ps.dirlist = nil');
-            Continue;
           end;
+          try
+            ps := TPazoSite(p.PazoSitesList[i]);
 
-          // dirlist task already added
-          if (ps.dirlist.dirlistadded) and (event <> kbeUPDATE) then
-            Continue;
+            // dirlist not available
+            if ps.dirlist = nil then
+            begin
+              Debug(dpError, section, 'ERROR: ps.dirlist = nil');
+              Continue;
+            end;
 
-          // Source site is PRE site for this group
-          if ps.status in [rssShouldPre, rssRealPre] then
-          begin
-            r.PredOnAnySite := True;
-            dlt := TPazoDirlistTask.Create(netname, channel, ps.Name, p, '', True);
-            irc_Addtext_by_key('PRECATCHSTATS', Format('<c7>[KB]</c> %s %s Dirlist added to : %s (PRESITE) from event %s', [section, rls, ps.Name, KBEventTypeToString(event)]));
-            ps.dirlist.dirlistadded := True;
-            AddTask(dlt, true);
-          end;
+            // dirlist task already added
+            if (ps.dirlist.dirlistadded) and (event <> kbeUPDATE) then
+              Continue;
 
-          // Source site is _not_ a PRE site for this group
-          if ps.status in [rssNotAllowedButItsThere, rssAllowed, rssComplete] then
-          begin
-            dlt := TPazoDirlistTask.Create(netname, channel, ps.Name, p, '', False);
-            irc_Addtext_by_key('PRECATCHSTATS', Format('<c7>[KB]</c> %s %s Dirlist added to : %s (NOT PRESITE) from event %s', [section, rls, ps.Name, KBEventTypeToString(event)]));
-            ps.dirlist.dirlistadded := True;
-            AddTask(dlt, true);
-          end;
+            // Source site is PRE site for this group
+            if ps.status in [rssShouldPre, rssRealPre] then
+            begin
+              r.PredOnAnySite := True;
+              dlt := TPazoDirlistTask.Create(netname, channel, ps.Name, p, '', True);
+              irc_Addtext_by_key('PRECATCHSTATS', Format('<c7>[KB]</c> %s %s Dirlist added to : %s (PRESITE) from event %s', [section, rls, ps.Name, KBEventTypeToString(event)]));
+              ps.dirlist.dirlistadded := True;
+              AddTask(dlt, true);
+            end;
 
-        except
-          on E: Exception do
-          begin
-            Debug(dpError, section, Format('[EXCEPTION] kb_Add add dirlist iterate: %s', [e.Message]));
-            continue;
+            // Source site is _not_ a PRE site for this group
+            if ps.status in [rssNotAllowedButItsThere, rssAllowed, rssComplete] then
+            begin
+              dlt := TPazoDirlistTask.Create(netname, channel, ps.Name, p, '', False);
+              irc_Addtext_by_key('PRECATCHSTATS', Format('<c7>[KB]</c> %s %s Dirlist added to : %s (NOT PRESITE) from event %s', [section, rls, ps.Name, KBEventTypeToString(event)]));
+              ps.dirlist.dirlistadded := True;
+              AddTask(dlt, true);
+            end;
+
+          except
+            on E: Exception do
+            begin
+              Debug(dpError, section, Format('[EXCEPTION] kb_Add add dirlist iterate: %s', [e.Message]));
+              continue;
+            end;
           end;
         end;
       end;

config/slftp.ini
@@ -466,6 +466,12 @@ mode=0
 # set to 1 if you want to add to KB / start race when a pretime is inserted
 add_to_kb_on_dbaddpre_insert=0
 
+[UDPConfig]
+EnableUDP=False
+IP=127.0.0.1
+Port=6969
+Password=passwd
+
 [mysql]
 # ip adress or hostname
 host=0

dbaddpre.pas
@@ -326,6 +326,19 @@ var
   rls_section: String;
   kb_entry: String;
   p: Integer;
+
+  function IsUDPEnabled: Boolean;
+  var
+    rawEnable: String;
+    udpIp: String;
+    udpPort: Integer;
+  begin
+    rawEnable := Trim(config.ReadString('UDPConfig', 'EnableUDP', 'False'));
+    udpIp := Trim(config.ReadString('UDPConfig', 'IP', ''));
+    udpPort := config.ReadInteger('UDPConfig', 'Port', 0);
+    Result := (SameText(rawEnable, 'True') or SameText(rawEnable, '1')) and
+      (udpIp <> '') and (udpPort >= 1) and (udpPort <= 65535);
+  end;
 begin
   Result := False;
 
@@ -344,7 +357,7 @@ begin
         //send event to kb_add to trigger race evaluation
         if rls_section <> '' then
           kb_Add(netname, channel, getAdminSiteName, rls_section, '', event, rls, '')
-        else if add_to_kb_on_dbaddpre_insert then
+        else if add_to_kb_on_dbaddpre_insert or IsUDPEnabled then
         begin
           rls_section := aSection;  // if the precatcher config has a fixed section ... I don't think it makes much sense, but it's possible
           if rls_section = '' then

pazo.pas
@@ -173,6 +173,11 @@ type
     FUniqueFileListOfRelease_cs: TSlCriticalSection2; //< Critical section for Add calls to @link(FUniqueFileListOfRelease)
     FUniqueFileListOfRelease: TDictionary<String, Int64>; //< Dictionary with files (including subdirs) and corresponding filesize (biggest value seen on any site) for this release, Key="dir + '/' + filename" and Value=filesize
     FPazoSFV: TPazoSFV;
+    FUDPEnabled: Boolean;
+    FUDPIp: String;
+    FUDPPort: Integer;
+    FUDPPassword: String;
+    FUDPConfigLoaded: Boolean;
 
     { Creates/Updates the filesize for given subdir and filename combination
       @param(aDir Location of the file inside releasedir)
@@ -181,6 +186,7 @@ type
     function PRegisterFile(const aDir: String; const de: TDirListEntry): Int64;
 
     procedure QueueEvent(Sender: TObject; Value: integer);
+    procedure LoadUDPConfig;
 
   public
     pazo_id: integer;
@@ -269,7 +275,8 @@ implementation
 uses
   SysUtils, StrUtils, mainthread, sitesunit, DateUtils, debugunit, queueunit,
   taskrace, mystrings, irc, sltcp, slhelper, Math, taskpretime, configunit,
-  mrdohutils, console, RegExpr, statsunit, Generics.Defaults, kb, tasksitesfv;
+  mrdohutils, console, RegExpr, statsunit, Generics.Defaults, kb, tasksitesfv,
+  mormot.core.base, mormot.core.unicode, mormot.net.sock;
 
 const
   section = 'pazo';
@@ -279,6 +286,34 @@ var
   glMaxBadcrcEvents: integer; //< max number of bad crc events read from config
   glPazoPreTimeLookupMode: TPretimeLookupMode;
   glShowCompleteTimeStats: boolean;
+
+function RlsStatusToString(const aStatus: TRlsSiteStatus): String;
+begin
+  case aStatus of
+    rssNotAllowed: Result := 'not_allowed';
+    rssNotAllowedButItsThere: Result := 'not_allowed_present';
+    rssAllowed: Result := 'allowed';
+    rssShouldPre: Result := 'should_pre';
+    rssRealPre: Result := 'real_pre';
+    rssComplete: Result := 'complete';
+    rssNuked: Result := 'nuked';
+  else
+    Result := 'unknown';
+  end;
+end;
+
+function SiteStatusToString(const aStatus: TSiteStatus): String;
+begin
+  case aStatus of
+    sstUnknown: Result := 'unknown';
+    sstUp: Result := 'up';
+    sstDown: Result := 'down';
+    sstTempDown: Result := 'tempdown';
+    sstMarkedAsDownByUser: Result := 'markeddown';
+  else
+    Result := 'other';
+  end;
+end;
 
 
 constructor TDestinationRank.Create(const aPazoSite: TPazoSite; const aRank: integer);
@@ -401,6 +436,46 @@ begin
   end;
 end;
 
+procedure TPazo.LoadUDPConfig;
+var
+  rawEnable: String;
+begin
+  FUDPEnabled := False;
+  FUDPConfigLoaded := False;
+  FUDPIp := '';
+  FUDPPort := 0;
+  FUDPPassword := '';
+
+  if not Assigned(config) then
+  begin
+    Debug(dpMessage, section, 'TPazo.LoadUDPConfig: config not initialised, retrying later');
+    Exit;
+  end;
+
+  rawEnable := Trim(config.ReadString('UDPConfig', 'EnableUDP', 'False'));
+  FUDPIp := Trim(config.ReadString('UDPConfig', 'IP', ''));
+  FUDPPort := config.ReadInteger('UDPConfig', 'Port', 0);
+  FUDPPassword := config.ReadString('UDPConfig', 'Password', '');
+  FUDPEnabled := SameText(rawEnable, 'True') or SameText(rawEnable, '1');
+
+  if FUDPEnabled then
+  begin
+    if (FUDPPort < 1) or (FUDPPort > 65535) then
+    begin
+      Debug(dpError, section, Format('TPazo.LoadUDPConfig: invalid UDP port %d - disabling UDP', [FUDPPort]));
+      FUDPEnabled := False;
+    end;
+
+    if FUDPIp = '' then
+    begin
+      Debug(dpError, section, 'TPazo.LoadUDPConfig: UDP IP is empty - disabling UDP');
+      FUDPEnabled := False;
+    end;
+  end;
+
+  FUDPConfigLoaded := True;
+end;
+
 function PazoAdd(const rls: TRelease): TPazo;
 begin
   Result := TPazo.Create(rls, local_pazo_id);
@@ -444,6 +519,8 @@ var
   s: TSite;
   fd: String;
 begin
+  if pazo.FUDPEnabled then exit(False);
+
   Result := False;
   dst := nil;
   dstdl := nil;
@@ -732,13 +809,55 @@ end;
 function TPazo.RoutesText: String;
 var
   ps: TPazoSite;
+  cbftpLine: String;
+  sitelist: String;
+  shouldSendUDP: Boolean;
+  udpSocket: TNetSocket;
+  udpMessage: RawUtf8;
+  destAddr: TNetAddr;
+  res: TNetResult;
 begin
+  if rls = nil then
+  begin
+    Result := '';
+    Exit;
+  end;
+
   Result := Format('<c3>[ROUTES]</c> : <b>%s</b> (%d sites)', [rls.rlsname, PazoSitesList.Count]);
   Result := Result + #13#10;
 
   for ps in PazoSitesList do
   begin
     Result := Result + ps.RoutesText;
+  end;
+
+  cbftpLine := '';
+  sitelist := '';
+  shouldSendUDP := False;
+
+  if not FUDPConfigLoaded then
+    LoadUDPConfig;
+
+  if FUDPEnabled then
+  begin
+    for ps in PazoSitesList do
+    begin
+      if (ps.status in [rssAllowed, rssShouldPre, rssRealPre]) then
+      begin
+        sitelist := sitelist + ps.Name + ',';
+      end;
+    end;
+
+    if sitelist <> '' then
+    begin
+      SetLength(sitelist, Length(sitelist) - 1);
+      cbftpLine := Format('<c3>[CBFTP]</c> : <b>%s %s</b> %s', [rls.section, rls.rlsname, sitelist]);
+      cbftpLine := cbftpLine + #13#10;
+      Result := Result + cbftpLine;
+      shouldSendUDP := True;
+    end
+    else
+      Debug(dpMessage, section, 'TPazo.RoutesText: no eligible sites for UDP, skipping send');
   end;
 
   if (Result <> lastannounceroutes) then
@@ -748,6 +867,58 @@ begin
   else
   begin
     Result := '';
+    shouldSendUDP := False;
+  end;
+
+  if shouldSendUDP then
+  begin
+    udpMessage := StringToUtf8(FUDPPassword + ' race ' + rls.section + ' ' + rls.rlsname + ' ' + sitelist);
+    udpSocket := nil;
+    try
+      // Create UDP socket
+      udpSocket := NewRawSocket(nfIP4, nlUdp);
+      if udpSocket = nil then
+      begin
+        Debug(dpError, section, 'UDP socket creation failed');
+        lastannounceroutes := '';
+        Exit;
+      end;
+
+      // Set send timeout
+      udpSocket.SetSendTimeout(2000);
+
+      // Set destination address
+      res := destAddr.SetFrom(StringToUtf8(FUDPIp), StringToUtf8(IntToStr(FUDPPort)), nlUdp);
+      if res <> nrOK then
+      begin
+        Debug(dpError, section, 'UDP address resolution failed: %s', [ToText(res)^]);
+        lastannounceroutes := '';
+        Exit;
+      end;
+
+      // Send UDP datagram
+      res := udpSocket.SendTo(pointer(udpMessage), length(udpMessage), destAddr);
+      if res <> nrOK then
+      begin
+        Debug(dpError, section, 'UDP send failed: %s', [ToText(res)^]);
+        lastannounceroutes := '';
+      end
+      else
+      begin
+        if FUDPEnabled then
+          irc_SendROUTEINFOS(Format('<c10>[<b>CBFTP</b>]</c> Sending to cbftp: %s %s %s', [rls.section, rls.rlsname, sitelist]));
+      end;
+    except
+      on E: Exception do
+      begin
+        Debug(dpError, section, Format('[EXCEPTION] TPazo.RoutesText: UDP operation failed: %s', [E.Message]));
+        lastannounceroutes := '';
+      end;
+    end;
+
+    // Cleanup
+    if udpSocket <> nil then
+      udpSocket.Close;
   end;
 end;
 
@@ -785,7 +956,7 @@ begin
     FUniqueFileListOfRelease_cs.Leave;
   end;
 
-  if fWasAdded And de.IsSFV and self.rls.IsSFVRelease and not FPazoSFV.HasSFV(aDir) then
+  if fWasAdded And de.IsSFV and self.rls.IsSFVRelease and not FPazoSFV.HasSFV(aDir) and not IsUDPEnabled then
   begin
     if FPazoSFV.RegisterSFV(aDir) then
     begin
@@ -812,7 +983,9 @@ end;
 
 function TPazo.IsUDPEnabled: Boolean;
 begin
-  Result := config.ReadBool('UDPConfig', 'EnableUDP', False);
+  if not FUDPConfigLoaded then
+    LoadUDPConfig;
+  Result := FUDPEnabled;
 end;
 
 constructor TPazo.Create(const rls: TRelease; const pazo_id: integer);
@@ -857,6 +1030,14 @@ begin
   FExcludeFromIncfiller := False;
   if rls.IsSFVRelease then
     FPazoSFV := TPazoSFV.Create;
+
+  FUDPConfigLoaded := False;
+  FUDPEnabled := False;
+  FUDPIp := '';
+  FUDPPort := 0;
+  FUDPPassword := '';
+
+  LoadUDPConfig;
 
   inherited Create;
 end;
@@ -930,27 +1111,32 @@ begin
           lastannounceconsole := s;
         end;
 
-        s := Stats(False, False);
-        if ((lastannounceirc <> s) and (s <> '')) then
+        // display race stats on irc (skip when UDP is enabled)
+        if not IsUDPEnabled then
         begin
-          irc_addstats(Format('<c10>[<b>STATS</b>]</c> %s %s (%d):', [rls.section, rls.rlsname, GetCountOfCachedFiles, s]));
-          irc_AddstatsB(Stats(False, True));
-          lastannounceirc := s;
-
-          if glShowCompleteTimeStats then
+          s := Stats(False, False);
+          if ((lastannounceirc <> s) and (s <> '')) then
           begin
-            s := SiteCompleteTimesStats(False);
-            if s <> '' then
+            irc_addstats(Format('<c10>[<b>STATS</b>]</c> %s %s (%d):', [rls.section, rls.rlsname, GetCountOfCachedFiles, s]));
+            irc_AddstatsB(Stats(False, True));
+            lastannounceirc := s;
+
+            if glShowCompleteTimeStats then
             begin
-              irc_addstats(Format('<c10>[<b>COMPLETE TIME AFTER ADDPRE</b>]</c> %s %s (%d):', [rls.section, rls.rlsname, GetCountOfCachedFiles]));
-              irc_addstats(s);
+              s := SiteCompleteTimesStats(False);
+              if s <> '' then
+              begin
+                irc_addstats(Format('<c10>[<b>COMPLETE TIME AFTER ADDPRE</b>]</c> %s %s (%d):', [rls.section, rls.rlsname, GetCountOfCachedFiles]));
+                irc_addstats(s);
+              end;
             end;
           end;
         end;
       end
       else
       begin
-        irc_addstats('<c10>[<b>STATS</b>]</c> Pazo Stopped.');
+        if not IsUDPEnabled then
+          irc_addstats('<c10>[<b>STATS</b>]</c> Pazo Stopped.');
       end;
     end;
   end;
@@ -1196,19 +1382,22 @@ var
   sectiondir: String;
   ps: TPazoSite;
 begin
+  if not FUDPConfigLoaded then
+    LoadUDPConfig;
+
   Result := False;
   for i := sitesunit.sites.Count - 1 downto 0 do
   begin
     try
       s := TSite(sitesunit.sites[i]);
-      if not (s.WorkingStatus in [sstUnknown, sstUp]) then
+      if (not FUDPEnabled) and (not (s.WorkingStatus in [sstUnknown, sstUp])) then
         Continue;
-      if s.PermDown then
+      if (not FUDPEnabled) and s.PermDown then
         Continue;
       if aIsSpreadJob then
       begin
         if s.SkipPre then
-          Continue;
+        Continue;
       end;
 
       sectiondir := s.sectiondir[rls.section];

rulesunit.pas
@@ -1648,9 +1648,9 @@ begin
   ps_s := FindSiteByName('', ps.Name);
   if ps_s = nil then
     exit;
-  if ps_s.PermDown then
+  if (not p.IsUDPEnabled) and ps_s.PermDown then
     exit;
-  if not (ps_s.WorkingStatus in [sstUnknown, sstUp]) then
+  if (not p.IsUDPEnabled) and not (ps_s.WorkingStatus in [sstUnknown, sstUp]) then
     exit;
 
   p.srcsite := ps.Name;
@@ -1693,7 +1693,7 @@ begin
         if dstps_s = nil then
           Continue;
 
-        if (not (dstps_s.WorkingStatus in [sstUnknown, sstUp])) or (dstps_s.PermDown) then
+        if (not p.IsUDPEnabled) and ((not (dstps_s.WorkingStatus in [sstUnknown, sstUp])) or (dstps_s.PermDown)) then
         begin
           if (dstps.reason = '') then
             dstps.reason := 'Down';

taskgenredirlist.pas
@@ -86,6 +86,13 @@ begin
     exit;
   end;
 
+  if mainpazo.IsUDPEnabled then
+  begin
+    ready := True;
+    Result := True;
+    exit;
+  end;
+
   Debug(dpMessage, section, Name);
 
   // mp3genre already known?

taskgenrenfo.pas
@@ -99,6 +99,13 @@ begin
     exit;
   end;
 
+  if mainpazo.IsUDPEnabled then
+  begin
+    ready := True;
+    Result := True;
+    exit;
+  end;
+
   // exit if nfo is already in dbaddnfo
   try
     i := last_addnfo.IndexOf(mainpazo.rls.rlsname);

tasksitenfo.pas
@@ -83,6 +83,13 @@ begin
     exit;
   end;
 
+  if mainpazo.IsUDPEnabled then
+  begin
+    ready := True;
+    Result := True;
+    exit;
+  end;
+
   // exit if imdb info is already known in last_imdbdata
   dbaddimdb_cs.Enter('TPazoSiteNfoTask.Execute');
   try

commit bed1e44ef39cd9f5fe4e9326ccbdd3f7cce7f3b9

fix(irc): restore ROUTEINFOS send with route scores when UDP disabled
Regression introduced by ffb6563d which ported UDP/cbftp routing but
dropped the irc_SendROUTEINFOS(ss) call from kb.pas. Restores the fix
originally applied in a7d5a37e: send full routes text (including
calculated ranks/scores) to ROUTEINFOS channels when UDP is disabled.
When UDP is enabled, RoutesText() already sends the CBFTP line internally.

kb.pas
@@ -824,6 +824,8 @@ begin
 
   // status changed
   ss := p.RoutesText;
+  if (ss <> '') and (not p.IsUDPEnabled) then
+    irc_SendROUTEINFOS(ss);
 
   if (psource <> nil) and (psource.Status = rssNotAllowed) then
   begin