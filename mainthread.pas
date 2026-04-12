cea06611e8fc5b818ed745c3fb7ccaa79f082a53
PES <pesdev@protonmail.com>
2026-04-12 22:07:05 +0200

    less log spam for kill_ghosts_on_startup but show info in admin channel

--- mainthread.pas
+++ mainthread.pas
@@ -310,13 +310,14 @@ begin
     glStartupGhostKillDone := True;
     if config.ReadBool('sites', 'kill_ghosts_on_startup', False) then
     begin
-      Debug(dpError, section, 'Startup: Triggering automatic ghost kill for all sites...');
+      Debug(dpMessage, section, 'Startup: Triggering automatic ghost kill for all sites...');
+      irc_addAdmin('Startup: Triggering automatic ghost kill for all sites...');
       for i := 0 to sites.Count - 1 do
       begin
         fSite := TSite(sites.Items[i]);
-        if (fSite.Name <> getAdminSiteName) then
+        if (fSite.Name <> getAdminSiteName) and not fSite.PermDown and not(fSite.WorkingStatus in [sstMarkedAsDownByUser, sstDown]) then
         begin
-          Debug(dpError, section, 'Startup: Adding ghost kill task for site %s', [fSite.Name]);
+          Debug(dpSpam, section, 'Startup: Adding ghost kill task for site %s', [fSite.Name]);
           AddTask(TLoginTask.Create('', '', fSite.Name, True, False));
           fSite.QueueFire;
         end;
