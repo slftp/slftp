ab8642fe221bd2f43e1c78223f8eedf804e9fb25
DEADC0DE <Dreamboxx@protonmail.com>
2026-05-07 13:58:54 +0200

    refactor(taskrace): store site-specific newdir_dirlist_readd in TPazoSite
    Eliminates FindSiteByName lookup from GetDirlistReaddValue which was
    called from the queue thread and caused race conditions against
    sitesDict modifications. The site-specific value is now loaded once
    when TPazoSite is created and read directly from the TPazoSite field.
    
    - pazo.pas: add newdir_dirlist_readd field to TPazoSite, populate in
      constructor from TSite.NewdirDirlistReadd
    - taskrace.pas: replace FindSiteByName call with direct field access

--- taskrace.pas
+++ taskrace.pas
@@ -751,17 +751,12 @@ function TPazoDirlistTask.GetDirlistReaddValue(aSite: TPazoSite; aDirlist: TDirL
 var
   baseValue: integer;
   secondsSinceLastChange: Int64;
-  fSite: TSite;
 begin
   baseValue := GetNewdirDirlistReaddValue();
 
   // Site-specific override takes precedence over global default
-  if aSite <> nil then
-  begin
-    fSite := FindSiteByName('', aSite.Name);
-    if (fSite <> nil) and (fSite.NewdirDirlistReadd > 0) then
-      baseValue := fSite.NewdirDirlistReadd;
-  end;
+  if (aSite <> nil) and (aSite.newdir_dirlist_readd > 0) then
+    baseValue := aSite.newdir_dirlist_readd;
 
   baseValue := GetNewdirDirlistReaddLoadAdjustedValue(baseValue);
 
