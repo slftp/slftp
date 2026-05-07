65d5a880421ffed2aa08ef1829a527bd5a11c021
Dreamboxx <Dreamboxx@protonmail.com>
2026-05-07 11:47:55 +0200

    fix(taskrace): nil-guard mainpazo.rls in Task.Name methods
    TPazoRaceTask.Name logged mainpazo=nil but then dereferenced mainpazo.rls
    without exiting, causing guaranteed AV. Same bug existed in
    TPazoDirlistTask.Name and TPazoMkdirTask.Name.
    
    Add early nil/rls checks with safe fallback strings before any
    mainpazo.rls access in all three Name methods. Prevents cascading AVs
    when AddTask exception handlers call t.Name on orphaned tasks.

--- taskrace.pas
+++ taskrace.pas
@@ -681,6 +681,12 @@ end;
 function TPazoDirlistTask.Name: String;
 begin
   try
+    if (mainpazo = nil) or (mainpazo.rls = nil) then
+    begin
+      Result := Format('DIRLIST : %d <b>%s</b> %s', [pazo_id, site1, dir]);
+      exit;
+    end;
+
     if is_pre then
       Result := Format('DIRLIST : %d <b>%s</b> <b>PRE</b> %s %s %s %s', [pazo_id, site1, mainpazo.rls.section,
         mainpazo.rls.rlsname, dir, ScheduleText])
@@ -1156,6 +1162,12 @@ end;
 function TPazoMkdirTask.Name: String;
 begin
   try
+    if (mainpazo = nil) or (mainpazo.rls = nil) then
+    begin
+      Result := Format('MKDIR : %d <b>%s</b> %s', [pazo_id, site1, dir]);
+      exit;
+    end;
+
     Result := Format('MKDIR : %d <b>%s</b> %s %s', [pazo_id, site1, mainpazo.rls.rlsname, dir]);
   except
     Result := 'MKDIR';
@@ -3493,6 +3505,14 @@ begin
       slotInfo := Format(' <c9>[%s]</c>', [slot2name]);
     if slotInfo = '' then
       siteInfo := Format(' <b>%s</b>-><b>%s</b>', [site1, site2]);
+
+    if mainpazo = nil then
+    begin
+      Debug(dpError, c_section, 'CRITICAL LOG: TPazoRaceTask.Name called but mainpazo is nil!');
+      Result := Format('<c7>[RACE]</c> #%d%s%s : <c10>%s</c> <c7>(%d)</c>',
+        [pazo_id, siteInfo, slotInfo, filename, rank]);
+      exit;
+    end;
 
     if mainpazo.rls = nil then
       Result := Format('<c7>[RACE]</c> #%d%s%s : <c10>%s</c> <c7>(%d)</c>',
