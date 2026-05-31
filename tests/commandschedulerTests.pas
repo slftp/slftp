055a7008a5a5ad213f95a59da8026bf9e96b9769
Moe Spinat <moe201484@protonmail.com>
2026-05-31 12:02:56 +0200

    test(scheduler): add unit tests for new command types
    - Tests for SFV, NFO, CWD, Raw, Login scheduling
    - Tests for SFV and Raw deduplication
    - Tests for command priority ordering
    - Tests for HasCommand, CompleteCommand, OtherCount
    - Tests for TotalCount and RemoveByPazo with mixed command types

--- tests/commandschedulerTests.pas
+++ tests/commandschedulerTests.pas
@@ -29,6 +29,19 @@ type
     procedure TestMixedDirlistAndMkdir;
     procedure TestCompleteDirlist;
     procedure TestCompleteMkdir;
+    procedure TestScheduleSfvDownload;
+    procedure TestScheduleNfoDownload;
+    procedure TestScheduleCwd;
+    procedure TestScheduleRaw;
+    procedure TestScheduleLogin;
+    procedure TestSfvDeduplication;
+    procedure TestRawDeduplication;
+    procedure TestGetNextCommandPriority;
+    procedure TestHasCommand;
+    procedure TestCompleteCommand;
+    procedure TestOtherCount;
+    procedure TestTotalCountWithOther;
+    procedure TestRemoveByPazoWithOther;
   end;
 
 implementation
@@ -406,6 +419,281 @@ begin
     CheckEquals(0, fSched.MkdirCount, 'Should have 0 mkdirs after complete');
 
     CheckFalse(fSched.GetNextMkdir(fOut), 'Should not get completed mkdir');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestScheduleSfvDownload;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(1, '/test/dir', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq.sfv_filename := 'test.sfv';
+    CheckTrue(fSched.ScheduleCommand(fReq), 'Should schedule SFV download');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestScheduleNfoDownload;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(1, '', 'TestSite', ctNfoDownload, 0, 'net', 'chan');
+    CheckTrue(fSched.ScheduleCommand(fReq), 'Should schedule NFO download');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestScheduleCwd;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(-1, '/test/dir', 'TestSite', ctCwd, 0, 'net', 'chan');
+    CheckTrue(fSched.ScheduleCommand(fReq), 'Should schedule CWD');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestScheduleRaw;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(-1, '/test/dir', 'TestSite', ctRaw, 0, 'net', 'chan');
+    fReq.cmd := 'SITE STAT';
+    CheckTrue(fSched.ScheduleCommand(fReq), 'Should schedule RAW');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestScheduleLogin;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(-1, '', 'TestSite', ctLogin, 0, 'net', 'chan');
+    CheckTrue(fSched.ScheduleCommand(fReq), 'Should schedule login');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestSfvDeduplication;
+var
+  fSched: TCommandScheduler;
+  fReq1, fReq2: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq1.Init(1, '/test/dir', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq1.sfv_filename := 'test.sfv';
+    CheckTrue(fSched.ScheduleCommand(fReq1), 'First SFV should succeed');
+
+    // Same pazo_id, same dir -> duplicate (dedup ignores sfv_filename)
+    fReq2.Init(1, '/test/dir', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq2.sfv_filename := 'other.sfv';
+    CheckFalse(fSched.ScheduleCommand(fReq2), 'Duplicate SFV should be rejected');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should still be 1');
+
+    // Different dir -> ok
+    fReq2.Init(1, '/test/dir2', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq2.sfv_filename := 'test.sfv';
+    CheckTrue(fSched.ScheduleCommand(fReq2), 'Different dir SFV should succeed');
+    CheckEquals(2, fSched.OtherCount, 'OtherCount should be 2');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestRawDeduplication;
+var
+  fSched: TCommandScheduler;
+  fReq1, fReq2: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq1.Init(-1, '/test/dir', 'TestSite', ctRaw, 0, 'net', 'chan');
+    fReq1.cmd := 'SITE STAT';
+    CheckTrue(fSched.ScheduleCommand(fReq1), 'First RAW should succeed');
+
+    // Same dir, same cmd -> duplicate
+    fReq2.Init(-1, '/test/dir', 'TestSite', ctRaw, 0, 'net', 'chan');
+    fReq2.cmd := 'SITE STAT';
+    CheckFalse(fSched.ScheduleCommand(fReq2), 'Duplicate RAW should be rejected');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should still be 1');
+
+    // Same dir, different cmd -> ok
+    fReq2.Init(-1, '/test/dir', 'TestSite', ctRaw, 0, 'net', 'chan');
+    fReq2.cmd := 'SITE USER';
+    CheckTrue(fSched.ScheduleCommand(fReq2), 'Different cmd RAW should succeed');
+    CheckEquals(2, fSched.OtherCount, 'OtherCount should be 2');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestGetNextCommandPriority;
+var
+  fSched: TCommandScheduler;
+  fReq1, fReq2, fOut: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    // Add with different priorities
+    fReq1.Init(-1, '/b', 'TestSite', ctCwd, 0, 'net', 'chan');
+    fReq1.priority := 2;
+    fSched.ScheduleCommand(fReq1);
+
+    fReq2.Init(-1, '/a', 'TestSite', ctCwd, 0, 'net', 'chan');
+    fReq2.priority := 1;
+    fSched.ScheduleCommand(fReq2);
+
+    CheckTrue(fSched.GetNextCommand(ctCwd, fOut), 'Should get a CWD');
+    CheckEquals('/a', fOut.dir, 'Should get /a first (priority 1)');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestHasCommand;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    CheckFalse(fSched.HasCommand(ctSfvDownload, 1, '/test'), 'Should not have SFV initially');
+
+    fReq.Init(1, '/test', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq.sfv_filename := 'test.sfv';
+    fSched.ScheduleCommand(fReq);
+
+    CheckTrue(fSched.HasCommand(ctSfvDownload, 1, '/test'), 'Should have SFV after schedule');
+    CheckFalse(fSched.HasCommand(ctNfoDownload, 1, '/test'), 'Should not have NFO');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestCompleteCommand;
+var
+  fSched: TCommandScheduler;
+  fReq, fOut: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(-1, '/test', 'TestSite', ctCwd, 0, 'net', 'chan');
+    fSched.ScheduleCommand(fReq);
+    CheckEquals(1, fSched.OtherCount, 'Should have 1 other');
+
+    fSched.CompleteCommand(fReq);
+    CheckEquals(0, fSched.OtherCount, 'Should have 0 others after complete');
+
+    CheckFalse(fSched.GetNextCommand(ctCwd, fOut), 'Should not get completed CWD');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestOtherCount;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    CheckEquals(0, fSched.OtherCount, 'OtherCount should be 0 initially');
+
+    fReq.Init(-1, '/test', 'TestSite', ctCwd, 0, 'net', 'chan');
+    fSched.ScheduleCommand(fReq);
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+
+    fReq.Init(-1, '/test2', 'TestSite', ctRaw, 0, 'net', 'chan');
+    fReq.cmd := 'SITE STAT';
+    fSched.ScheduleCommand(fReq);
+    CheckEquals(2, fSched.OtherCount, 'OtherCount should be 2');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestTotalCountWithOther;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(1, '/test', 'TestSite', ctDirlist, 0, 'net', 'chan');
+    fSched.ScheduleDirlist(fReq);
+
+    fReq.Init(1, '/test', 'TestSite', ctMkdir, 0, 'net', 'chan');
+    fSched.ScheduleMkdir(fReq);
+
+    fReq.Init(-1, '/test', 'TestSite', ctCwd, 0, 'net', 'chan');
+    fSched.ScheduleCommand(fReq);
+
+    CheckEquals(1, fSched.DirlistCount, 'DirlistCount should be 1');
+    CheckEquals(1, fSched.MkdirCount, 'MkdirCount should be 1');
+    CheckEquals(1, fSched.OtherCount, 'OtherCount should be 1');
+    CheckEquals(3, fSched.TotalCount, 'TotalCount should be 3');
+  finally
+    fSched.Free;
+  end;
+end;
+
+procedure TTestCommandScheduler.TestRemoveByPazoWithOther;
+var
+  fSched: TCommandScheduler;
+  fReq: TCommandRequest;
+begin
+  fSched := TCommandScheduler.Create('TestSite');
+  try
+    fReq.Init(1, '/test1', 'TestSite', ctDirlist, 0, 'net', 'chan');
+    fSched.ScheduleDirlist(fReq);
+
+    fReq.Init(1, '/test1', 'TestSite', ctMkdir, 0, 'net', 'chan');
+    fSched.ScheduleMkdir(fReq);
+
+    fReq.Init(1, '/test1', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq.sfv_filename := 'test.sfv';
+    fSched.ScheduleCommand(fReq);
+
+    fReq.Init(2, '/test2', 'TestSite', ctSfvDownload, 0, 'net', 'chan');
+    fReq.sfv_filename := 'test2.sfv';
+    fSched.ScheduleCommand(fReq);
+
+    CheckEquals(1, fSched.DirlistCount, 'Should have 1 dirlist');
+    CheckEquals(1, fSched.MkdirCount, 'Should have 1 mkdir');
+    CheckEquals(2, fSched.OtherCount, 'Should have 2 others');
+
+    fSched.RemoveByPazo(1);
+    CheckEquals(0, fSched.DirlistCount, 'Should have 0 dirlists after remove');
+    CheckEquals(0, fSched.MkdirCount, 'Should have 0 mkdirs after remove');
+    CheckEquals(1, fSched.OtherCount, 'Should have 1 other after remove');
   finally
     fSched.Free;
   end;
