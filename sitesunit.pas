1ec03f7e4b97fa1dbe3af2b910b4a17b3040a271
Moe Spinat <moe201484@protonmail.com>
2026-06-14 17:34:42 +0200

    Re-enable freeslots pre-checks and fix TSite.FullLogin freeslots bug
    The temporary disabling of freeslots checks increased lock contention
    and worsened throughput, proving they are protective. Re-enable them
    in TryToAssignRaceSlots, TryToAssignSlots, AddTask and the main queue
    loop.
    
    Also fix TSite.FullLogin which unconditionally set ffreeslots := 0
    after iterating slots, leaving a site appearing slot-less until a
    later slot transition recalculated the value. Now it calls
    RecalcFreeslots so the cached count is correct immediately.

--- sitesunit.pas
+++ sitesunit.pas
@@ -4541,9 +4541,7 @@ procedure TSite.FullLogin;
 var
   i: integer;
   ss: TSiteSlot;
-  fs: integer;
 begin
-  fs := 0;
   for i := 0 to slots.Count - 1 do
   begin
     ss := TSiteSlot(slots[i]);
@@ -4553,7 +4551,7 @@ begin
     end;
   end;
 
-  ffreeslots := fs;
+  RecalcFreeslots;
 end;
 
 procedure TSite.RebuildSlot(const aSlotNumber: integer);
